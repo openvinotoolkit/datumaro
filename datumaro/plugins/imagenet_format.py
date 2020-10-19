312df5e899857fcff50af90f088d38b30e1ab5bb
yasakova-anastasia <anastasia.yasakova@intel.com>
2020-10-19 17:25:22 +0300

    some fixes

--- datumaro/plugins/imagenet_format.py
+++ datumaro/plugins/imagenet_format.py
@@ -2,7 +2,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-from collections import defaultdict
 import os
 import os.path as osp
 from glob import glob
@@ -11,7 +10,6 @@ import logging as log
 from datumaro.components.extractor import (DatasetItem, Label,
     LabelCategories, AnnotationType, SourceExtractor, Importer
 )
-from datumaro.util.log_utils import logging_disabled
 from datumaro.components.converter import Converter
 
 
@@ -23,18 +21,11 @@ class ImagenetPath:
 class ImagenetExtractor(SourceExtractor):
     def __init__(self, path):
         assert osp.isfile(path), path
-        self._path = path
-        self._dataset_dir = osp.dirname(path)
 
         super().__init__(subset=osp.splitext(osp.basename(path))[0])
 
         labels = osp.join(osp.dirname(path), ImagenetPath.LABELS_FILE)
-        if isinstance(labels, str):
-            labels = self._parse_labels(labels)
-        elif isinstance(labels, list):
-            assert all(isinstance(lbl, str) for lbl in labels), labels
-        else:
-            raise TypeError("Unexpected type of 'labels' argument: %s" % labels)
+        labels = self._parse_labels(labels)
 
         self._categories = self._load_categories(labels)
         self._items = list(self._load_items(path).values())
@@ -52,53 +43,34 @@ class ImagenetExtractor(SourceExtractor):
         return { AnnotationType.label: label_cat }
 
     def _load_items(self, path):
-        items = defaultdict(dict)
-
+        items = {}
+        images_path = osp.join(osp.dirname(path), ImagenetPath.IMAGES_DIR)
         with open(path, encoding='utf-8') as f:
             for line in f:
-                image_name, label_id = line.split()
+                item = line.split()
+                image_name = item[0]
+                labels_id = item[1:]
+                image_path = osp.join(images_path,
+                    image_name[:-(len(image_name.split('_')[-1]) + 1)], image_name)
+                anno = []
+                for label_id in labels_id:
+                    anno += [Label(label=label_id)]
                 items[image_name[:-4]] = DatasetItem(
                     id=image_name[:-4], subset=self._subset,
-                    image=image_name,
-                    annotations=[Label(label=label_id)])
+                    image=image_path, annotations=anno
+                )
         return items
 
 
 class ImagenetImporter(Importer):
-
     @classmethod
-    def detect(cls, path):
-        with logging_disabled(log.WARN):
-            return (len(cls.find_sources(path)) != 0)
-
-    def __call__(self, path, **extra_params):
-        from datumaro.components.project import Project # cyclic import
-        project = Project()
-
-        subsets = self.find_sources(path)
-
-        if len(subsets) == 0:
-            raise Exception("Failed to find 'imagenet' dataset at '%s'" % path)
-
-        for ann_name, ann_file in subsets.items():
-            project.add_source(ann_name, {
-                    'url': ann_file,
-                    'format': 'imagenet',
-                    'options': dict(extra_params),
-                })
-
-        return project
-
-    @staticmethod
-    def find_sources(path):
-        subsets = defaultdict(dict)
+    def find_sources(cls, path):
         subset_paths = [p for p in glob(osp.join(path, '*.txt'))
-                if 'synsets' not in osp.basename(p)]
-
+                 if 'synsets' not in osp.basename(p)]
+        sources = []
         for subset_path in subset_paths:
-            subset_name = osp.splitext(osp.basename(subset_path))[0]
-            subsets[subset_name] = subset_path
-        return subsets
+            sources += cls._find_sources_recursive(subset_path, '.txt', 'imagenet')
+        return sources
 
 
 class ImagenetConverter(Converter):
@@ -110,26 +82,35 @@ class ImagenetConverter(Converter):
         images_dir = osp.join(subset_dir, ImagenetPath.IMAGES_DIR)
         os.makedirs(images_dir, exist_ok=True)
         self._images_dir = images_dir
-        image_labels = defaultdict(dict)
+        image_labels = {}
         for subset_name, subset in self._extractor.subsets().items():
             annotation_file = osp.join(subset_dir, '%s.txt' % subset_name)
             annotation = ''
             for item in subset:
-                label = item.annotations[0].label
-                if label not in image_labels:
-                    image_dir = osp.join(images_dir, item.id[:-(len(item.id.split('_')[-1]) + 1)])
-                    os.makedirs(image_dir, exist_ok=True)
-                    image_labels[label] = image_dir
-                if not item.has_image:
-                    raise Exception("Failed to export item '%s': "
-                        "item has no image info" % item.id)
                 image_name = self._make_image_filename(item)
+                if len(item.annotations) == 1:
+                    label = item.annotations[0].label
+                    if label not in image_labels:
+                        image_dir = osp.join(images_dir, item.id[:-(len(item.id.split('_')[-1]) + 1)])
+                        os.makedirs(image_dir, exist_ok=True)
+                        image_labels[label] = image_dir
+                    annotation += '%s %s\n' % (image_name, label)
+                else:
+                    label = -1
+                    if label not in image_labels:
+                        image_dir = osp.join(images_dir, 'others')
+                        os.makedirs(image_dir, exist_ok=True)
+                        image_labels[label] = image_dir
+                    annotation += '%s' % image_name
+                    for anno in item.annotations:
+                        annotation += ' %s' % anno.label
+                    annotation += '\n'
+
                 if self._save_images:
                     if item.has_image and item.image.has_data:
                         self._save_image(item, osp.join(image_labels[label], image_name))
                     else:
                         log.warning("Item '%s' has no image" % item.id)
-                annotation += '%s %s\n' % (image_name, label)
             with open(annotation_file, 'w', encoding='utf-8') as f:
                 f.write(annotation)
 
--- tests/test_imagenet_format.py
+++ tests/test_imagenet_format.py
@@ -53,6 +53,49 @@ class ImagenetFormatTest(TestCase):
 
             compare_datasets(self, source_dataset, parsed_dataset)
 
+    def test_can_save_and_load_multiple_labels(self):
+        source_dataset = Dataset.from_iterable([
+            DatasetItem(id='1', subset='train',
+                image=np.ones((8, 8, 3)),
+                annotations=[Label(1), Label(3)]
+            ),
+            DatasetItem(id='label_0_1', subset='train',
+                image=np.ones((10, 10, 3)),
+                annotations=[Label(1)]
+            ),
+            DatasetItem(id='2', subset='train',
+                image=np.ones((10, 10, 3)),
+            ),
+        ], categories={
+            AnnotationType.label: LabelCategories.from_iterable(
+                'label_' + str(label) for label in range(10)),
+        })
+
+        with TestDir() as test_dir:
+            ImagenetConverter.convert(source_dataset, test_dir, save_images=True)
+
+            parsed_dataset = ImagenetImporter()(test_dir).make_dataset()
+
+            compare_datasets(self, source_dataset, parsed_dataset)
+
+    def test_can_save_dataset_with_no_subsets(self):
+        source_dataset = Dataset.from_iterable([
+            DatasetItem(id='a/b/c',
+                image=np.ones((8, 8, 3)),
+                annotations=[Label(1)]
+            ),
+        ], categories={
+            AnnotationType.label: LabelCategories.from_iterable(
+                'label_' + str(label) for label in range(10)),
+        })
+
+        with TestDir() as test_dir:
+            ImagenetConverter.convert(source_dataset, test_dir, save_images=True)
+
+            parsed_dataset = ImagenetImporter()(test_dir).make_dataset()
+
+            compare_datasets(self, source_dataset, parsed_dataset)
+
 DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'imagenet_dataset')
 
 class ImagenetImporterTets(TestCase):
