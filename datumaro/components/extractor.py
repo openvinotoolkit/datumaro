1efe5806a614f7bd06e54908d428287559f2f166
yasakova-anastasia <anastasia.yasakova@intel.com>
2021-01-19 18:13:49 +0300

    file filter include inside of extend(...)

--- datumaro/components/extractor.py
+++ datumaro/components/extractor.py
@@ -623,9 +623,9 @@ class Importer:
             sources = []
             for d in range(max_depth + 1):
                 sources.extend({'url': p, 'format': extractor_name} for p in
-                    iglob(osp.join(path, *('*' * d), dirname, filename + ext)))
-                if callable(file_filter):
-                    sources = [s for s in sources if file_filter(s['url'])]
+                    iglob(osp.join(path, *('*' * d), dirname, filename + ext))
+                    if (callable(file_filter) and file_filter(p)) \
+                    or (not callable(file_filter)))
                 if sources:
                     break
         return sources
