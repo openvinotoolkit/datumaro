abd7376665d244a55fe4a6aab4e564ece1ed8a2d
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-02-11 10:30:23 +0300

    update test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -129,6 +129,8 @@ class DatasetTest(TestCase):
 
             imported_dataset = Dataset.import_from(test_dir, env=env)
 
+            self.assertEqual(imported_dataset.data_path, test_dir)
+            self.assertEqual(imported_dataset.format, DEFAULT_FORMAT)
             compare_datasets(self, source_dataset, imported_dataset)
 
     def test_can_export_by_string_format_name(self):
