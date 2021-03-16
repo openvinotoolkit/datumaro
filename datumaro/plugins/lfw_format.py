1f084915537066cedcda5ff93f88ebbf689514c8
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-03-16 18:12:48 +0300

    support in lfw

--- datumaro/plugins/lfw_format.py
+++ datumaro/plugins/lfw_format.py
@@ -9,6 +9,7 @@ import re
 from datumaro.components.converter import Converter
 from datumaro.components.extractor import (AnnotationType, DatasetItem,
     Importer, Points, SourceExtractor)
+from datumaro.util.image import find_images
 
 
 class LfwPath:
@@ -32,7 +33,14 @@ class LfwExtractor(SourceExtractor):
 
     def _load_items(self, path):
         items = {}
+
         images_dir = osp.join(self._dataset_dir, self._subset, LfwPath.IMAGES_DIR)
+        if osp.isdir(images_dir):
+            images = { osp.splitext(osp.relpath(p, images_dir))[0]: p
+                for p in find_images(images_dir, recursive=True) }
+        else:
+            images = {}
+
         with open(path, encoding='utf-8') as f:
             for line in f:
                 pair = line.strip().split()
@@ -41,29 +49,29 @@ class LfwExtractor(SourceExtractor):
                     image2 = self.get_image_name(pair[0], pair[2])
                     if image1 not in items:
                         items[image1] = DatasetItem(id=image1, subset=self._subset,
-                            image=osp.join(images_dir, image1 + LfwPath.IMAGE_EXT),
+                            image=images.get(image1),
                             attributes={'positive_pairs': [], 'negative_pairs': []})
                     if image2 not in items:
                         items[image2] = DatasetItem(id=image2, subset=self._subset,
-                            image=osp.join(images_dir, image2 + LfwPath.IMAGE_EXT),
+                            image=images.get(image2),
                             attributes={'positive_pairs': [], 'negative_pairs': []})
 
-                    attributes = items[image1].attributes
-                    attributes['positive_pairs'].append(image2)
+                    # pairs form a directed graph
+                    items[image1].attributes['positive_pairs'].append(image2)
                 elif len(pair) == 4:
                     image1 = self.get_image_name(pair[0], pair[1])
                     image2 = self.get_image_name(pair[2], pair[3])
                     if image1 not in items:
                         items[image1] = DatasetItem(id=image1, subset=self._subset,
-                            image=osp.join(images_dir, image1 + LfwPath.IMAGE_EXT),
+                            image=images.get(image1),
                             attributes={'positive_pairs': [], 'negative_pairs': []})
                     if image2 not in items:
                         items[image2] = DatasetItem(id=image2, subset=self._subset,
-                            image=osp.join(images_dir, image2 + LfwPath.IMAGE_EXT),
+                            image=images.get(image2),
                             attributes={'positive_pairs': [], 'negative_pairs': []})
 
-                    attributes = items[image1].attributes
-                    attributes['negative_pairs'].append(image2)
+                    # pairs form a directed graph
+                    items[image1].attributes['negative_pairs'].append(image2)
 
         landmarks_file = osp.join(self._dataset_dir, self._subset,
             LfwPath.LANDMARKS_FILE)
@@ -72,9 +80,7 @@ class LfwExtractor(SourceExtractor):
                 for line in f:
                     line = line.split('\t')
 
-                    item_id = line[0]
-                    if item_id.endswith(LfwPath.IMAGE_EXT):
-                        item_id = item_id[:-len(LfwPath.IMAGE_EXT)]
+                    item_id = osp.splitext(line[0])[0]
                     if item_id not in items:
                         items[item_id] = DatasetItem(id=item_id, subset=self._subset,
                             image=osp.join(images_dir, line[0]),
@@ -82,6 +88,7 @@ class LfwExtractor(SourceExtractor):
 
                     annotations = items[item_id].annotations
                     annotations.append(Points([float(p) for p in line[1:]]))
+
         return items
 
     @staticmethod
@@ -94,17 +101,18 @@ class LfwImporter(Importer):
         return cls._find_sources_recursive(path, LfwPath.PAIRS_FILE, 'lfw')
 
 class LfwConverter(Converter):
-    DEFAULT_IMAGE_EXT = '.jpg'
+    DEFAULT_IMAGE_EXT = LfwPath.IMAGE_EXT
 
     def apply(self):
         for subset_name, subset in self._extractor.subsets().items():
             positive_pairs = []
             negative_pairs = []
             landmarks = []
+
             for item in subset:
-                if item.has_image and self._save_images:
-                    self._save_image(item, osp.join(self._save_dir, subset_name,
-                        LfwPath.IMAGES_DIR, item.id + LfwPath.IMAGE_EXT))
+                if self._save_images and item.has_image:
+                    self._save_image(item,
+                        subdir=osp.join(subset_name, LfwPath.IMAGES_DIR))
 
                 person1, num1 = LfwPath.PATTERN.search(item.id).groups()
                 num1 = int(num1)
