96e8cc39f7167dc9449e7511a8e024ae9a9c8e2e
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-03-25 15:28:51 +0300

    Update tests/test_splitter.py

--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -219,7 +219,11 @@ class SplitterTest(TestCase):
         }
         source = self._generate_dataset(config)
         splits = [("train", 0.1), ("val", 0.9), ("test", 0.0)]
+        
         actual = splitter.ClassificationSplit(source, splits)
+        
+        self.assertEqual(1, len(actual.get_subset("train")))
+        self.assertEqual(4, len(actual.get_subset("val")))
         self.assertEqual(0, len(actual.get_subset("test")))
 
     def test_split_for_classification_gives_error(self):
