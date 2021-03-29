b8d43b318179a8d20f6ddb1ae4a1bdd92b4399e6
Yi, Jihyeon <jihyeon.yi@intel.com>
2021-03-29 19:02:57 +0900

    validator threshold adjustment + style correction

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -19,26 +19,27 @@ from datumaro.components.extractor import Bbox, Label
 from datumaro.components.validator import (ClassificationValidator,
     DetectionValidator, TaskType, validate_annotations, _Validator)
 
+
 class TestValidatorTemplate(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.dataset = Dataset.from_iterable([
             DatasetItem(id=1, image=np.ones((5, 5, 3)), annotations=[
-                Label(1, id=0, attributes={ 'a': 1, 'b': 7, }),
+                Label(1, id=0, attributes={'a': 1, 'b': 7, }),
                 Bbox(1, 2, 3, 4, id=1, label=0, attributes={
                     'a': 1, 'b': 2,
                 }),
             ]),
             DatasetItem(id=2, image=np.ones((2, 4, 3)), annotations=[
-                Label(2, id=0, attributes={ 'a': 2, 'b': 2, }),
+                Label(2, id=0, attributes={'a': 2, 'b': 2, }),
                 Bbox(2, 3, 1, 4, id=1, label=0, attributes={
                     'a': 1, 'b': 1,
                 }),
             ]),
             DatasetItem(id=3),
             DatasetItem(id=4, image=np.ones((2, 4, 3)), annotations=[
-                Label(0, id=0, attributes={ 'b': 4, }),
-                Label(1, id=1, attributes={ 'a': 11, 'b': 7, }),
+                Label(0, id=0, attributes={'b': 4, }),
+                Label(1, id=1, attributes={'a': 11, 'b': 7, }),
                 Bbox(1, 3, 2, 4, id=2, label=0, attributes={
                     'a': 2, 'b': 1,
                 }),
@@ -47,30 +48,30 @@ class TestValidatorTemplate(TestCase):
                 }),
             ]),
             DatasetItem(id=5, image=np.ones((2, 4, 3)), annotations=[
-                Label(0, id=0, attributes={ 'a': 20, 'b': 10 }),
+                Label(0, id=0, attributes={'a': 20, 'b': 10, }),
                 Bbox(1, 2, 3, 4, id=1, label=1, attributes={
                     'a': 1, 'b': 1,
                 }),
             ]),
             DatasetItem(id=6, image=np.ones((2, 4, 3)), annotations=[
-                Label(1, id=0, attributes={ 'a': 11, 'b': 2, 'c': 3}),
+                Label(1, id=0, attributes={'a': 11, 'b': 2, 'c': 3, }),
                 Bbox(2, 3, 4, 1, id=1, label=1, attributes={
                     'a': 2, 'b': 2,
                 }),
             ]),
             DatasetItem(id=7, image=np.ones((2, 4, 3)), annotations=[
-                Label(1, id=0, attributes={ 'a': 1, 'b': 2, 'c': 5 }),
+                Label(1, id=0, attributes={'a': 1, 'b': 2, 'c': 5, }),
                 Bbox(1, 2, 3, 4, id=1, label=2, attributes={
                     'a': 1, 'b': 2,
                 }),
             ]),
             DatasetItem(id=8, image=np.ones((2, 4, 3)), annotations=[
-                Label(2, id=0, attributes={ 'a': 7, 'b': 9, 'c': 5 }),
+                Label(2, id=0, attributes={'a': 7, 'b': 9, 'c': 5, }),
                 Bbox(2, 1, 3, 4, id=1, label=2, attributes={
                     'a': 2, 'b': 1,
                 }),
             ]),
-        ], categories=[[f'label_{i}', None, { 'a', 'b' }] \
+        ], categories=[[f'label_{i}', None, {'a', 'b', }]
             for i in range(2)])
 
 
@@ -124,7 +125,7 @@ class TestBaseValidator(TestValidatorTemplate):
         label_name = 'unit'
         attr_name = 'test'
         attr_dets = {
-            'items_with_undefined_attr':[(1, 'unittest')]
+            'items_with_undefined_attr': [(1, 'unittest')]
         }
 
         actual_reports = self.validator._check_undefined_attribute(
@@ -193,64 +194,122 @@ class TestBaseValidator(TestValidatorTemplate):
         self.assertIsInstance(actual_reports[0], OnlyOneAttributeValue)
 
     def test_check_few_samples_in_label(self):
-        stats = {
-            'label_distribution': {
-                'defined_labels': {
-                    'unit': 1
+        with self.subTest('Few Samples'):
+            stats = {
+                'label_distribution': {
+                    'defined_labels': {
+                        'unit': self.validator.DEFAULT_FEW_SAMPLES
+                    }
                 }
             }
-        }
 
-        actual_reports = self.validator._check_few_samples_in_label(stats, 2)
+            actual_reports = self.validator._check_few_samples_in_label(stats)
 
-        self.assertTrue(len(actual_reports) == 1)
-        self.assertIsInstance(actual_reports[0], FewSamplesInLabel)
+            self.assertTrue(len(actual_reports) == 1)
+            self.assertIsInstance(actual_reports[0], FewSamplesInLabel)
+
+        with self.subTest('No Few Samples Warning'):
+            stats = {
+                'label_distribution': {
+                    'defined_labels': {
+                        'unit': self.validator.DEFAULT_FEW_SAMPLES + 1
+                    }
+                }
+            }
+
+            actual_reports = self.validator._check_few_samples_in_label(stats)
+
+            self.assertTrue(len(actual_reports) == 0)
 
     def test_check_few_samples_in_attribute(self):
         label_name = 'unit'
         attr_name = 'test'
-        attr_dets = {
-            'distribution': {
-                'mock': 1
-            }
-        }
 
-        actual_reports = self.validator._check_few_samples_in_attribute(
-            label_name, attr_name, attr_dets, 2)
-
-        self.assertTrue(len(actual_reports) == 1)
-        self.assertIsInstance(actual_reports[0], FewSamplesInAttribute)
-
-    def test_check_imbalanced_labels(self):
-        stats = {
-            'label_distribution': {
-                'defined_labels': {
-                    'unit': 5,
-                    'test': 1
+        with self.subTest('Few Samples'):
+            attr_dets = {
+                'distribution': {
+                    'mock': self.validator.DEFAULT_FEW_SAMPLES
                 }
             }
-        }
 
-        actual_reports = self.validator._check_imbalanced_labels(stats, 2)
+            actual_reports = self.validator._check_few_samples_in_attribute(
+                label_name, attr_name, attr_dets)
 
-        self.assertTrue(len(actual_reports) == 1)
-        self.assertIsInstance(actual_reports[0], ImbalancedLabels)
+            self.assertTrue(len(actual_reports) == 1)
+            self.assertIsInstance(actual_reports[0], FewSamplesInAttribute)
+
+        with self.subTest('No Few Samples Warning'):
+            attr_dets = {
+                'distribution': {
+                    'mock': self.validator.DEFAULT_FEW_SAMPLES + 1
+                }
+            }
+
+            actual_reports = self.validator._check_few_samples_in_attribute(
+                label_name, attr_name, attr_dets)
+
+            self.assertTrue(len(actual_reports) == 0)
+
+    def test_check_imbalanced_labels(self):
+        with self.subTest('Imbalance'):
+            stats = {
+                'label_distribution': {
+                    'defined_labels': {
+                        'unit': self.validator.DEFAULT_IMBALANCE_RATIO,
+                        'test': 1
+                    }
+                }
+            }
+
+            actual_reports = self.validator._check_imbalanced_labels(stats)
+
+            self.assertTrue(len(actual_reports) == 1)
+            self.assertIsInstance(actual_reports[0], ImbalancedLabels)
+
+        with self.subTest('No Imbalance Warning'):
+            stats = {
+                'label_distribution': {
+                    'defined_labels': {
+                        'unit': self.validator.DEFAULT_IMBALANCE_RATIO - 1,
+                        'test': 1
+                    }
+                }
+            }
+
+            actual_reports = self.validator._check_imbalanced_labels(stats)
+
+            self.assertTrue(len(actual_reports) == 0)
 
     def test_check_imbalanced_attribute(self):
         label_name = 'unit'
         attr_name = 'test'
-        attr_dets = {
-            'distribution': {
-                'mock': 5,
-                'mock_1': 1
+
+        with self.subTest('Imbalance'):
+            attr_dets = {
+                'distribution': {
+                    'mock': self.validator.DEFAULT_IMBALANCE_RATIO,
+                    'mock_1': 1
+                }
             }
-        }
 
-        actual_reports = self.validator._check_imbalanced_attribute(
-            label_name, attr_name, attr_dets, 2)
+            actual_reports = self.validator._check_imbalanced_attribute(
+                label_name, attr_name, attr_dets)
 
-        self.assertTrue(len(actual_reports) == 1)
-        self.assertIsInstance(actual_reports[0], ImbalancedAttribute)
+            self.assertTrue(len(actual_reports) == 1)
+            self.assertIsInstance(actual_reports[0], ImbalancedAttribute)
+
+        with self.subTest('No Imbalance Warning'):
+            attr_dets = {
+                'distribution': {
+                    'mock': self.validator.DEFAULT_IMBALANCE_RATIO - 1,
+                    'mock_1': 1
+                }
+            }
+
+            actual_reports = self.validator._check_imbalanced_attribute(
+                label_name, attr_name, attr_dets)
+
+            self.assertTrue(len(actual_reports) == 0)
 
 
 class TestClassificationValidator(TestValidatorTemplate):
@@ -286,38 +345,74 @@ class TestDetectionValidator(TestValidatorTemplate):
 
     def test_check_imbalanced_bbox_dist_in_label(self):
         label_name = 'unittest'
-        bbox_label_stats = {
-            'x': {
-                'histogram': {
-                    'counts': [1]
+        most = int(self.validator.DEFAULT_BBOX_IMBALANCE * 100)
+        rest = 100 - most
+
+        with self.subTest('Imbalanced'):
+            bbox_label_stats = {
+                'x': {
+                    'histogram': {
+                        'counts': [most, rest]
+                    }
                 }
             }
-        }
+            reports = self.validator._check_imbalanced_bbox_dist_in_label(
+                label_name, bbox_label_stats)
 
-        actual_reports = self.validator._check_imbalanced_bbox_dist_in_label(
-            label_name, bbox_label_stats, 0.9, 1)
+            self.assertTrue(len(reports) == 1)
+            self.assertIsInstance(reports[0], ImbalancedBboxDistInLabel)
 
-        self.assertTrue(len(actual_reports) == 1)
-        self.assertIsInstance(actual_reports[0], ImbalancedBboxDistInLabel)
+        with self.subTest('No Imbalanced Warning'):
+            bbox_label_stats = {
+                'x': {
+                    'histogram': {
+                        'counts': [most - 1, rest]
+                    }
+                }
+            }
+            reports = self.validator._check_imbalanced_bbox_dist_in_label(
+                label_name, bbox_label_stats)
+
+            self.assertTrue(len(reports) == 0)
 
     def test_check_imbalanced_bbox_dist_in_attr(self):
         label_name = 'unit'
         attr_name = 'test'
-        bbox_attr_stats = {
-            'mock': {
-                'x': {
-                    'histogram': {
-                        'counts': [1]
+        most = int(self.validator.DEFAULT_BBOX_IMBALANCE * 100)
+        rest = 100 - most
+
+        with self.subTest('Imbalanced'):
+            bbox_attr_stats = {
+                'mock': {
+                    'x': {
+                        'histogram': {
+                            'counts': [most, rest]
+                        }
                     }
                 }
             }
-        }
 
-        actual_reports = self.validator._check_imbalanced_bbox_dist_in_attr(
-            label_name, attr_name, bbox_attr_stats, 0.9, 1)
+            reports = self.validator._check_imbalanced_bbox_dist_in_attr(
+                label_name, attr_name, bbox_attr_stats)
 
-        self.assertTrue(len(actual_reports) == 1)
-        self.assertIsInstance(actual_reports[0], ImbalancedBboxDistInAttribute)
+            self.assertTrue(len(reports) == 1)
+            self.assertIsInstance(reports[0], ImbalancedBboxDistInAttribute)
+
+        with self.subTest('No Imbalanced Warning'):
+            bbox_attr_stats = {
+                'mock': {
+                    'x': {
+                        'histogram': {
+                            'counts': [most - 1, rest]
+                        }
+                    }
+                }
+            }
+
+            reports = self.validator._check_imbalanced_bbox_dist_in_attr(
+                label_name, attr_name, bbox_attr_stats)
+
+            self.assertTrue(len(reports) == 0)
 
     def test_check_missing_bbox_annotation(self):
         stats = {
@@ -358,7 +453,6 @@ class TestDetectionValidator(TestValidatorTemplate):
 
         self.assertTrue(len(actual_reports) == 1)
         self.assertIsInstance(actual_reports[0], InvalidValue)
-
 
     def test_check_far_from_label_mean(self):
         label_name = 'unittest'
@@ -406,7 +500,7 @@ class TestValidateAnnotations(TestValidatorTemplate):
     def test_validate_annotations_classification(self):
         actual_results = validate_annotations(self.dataset, 'classification')
 
-        with self.subTest('Test of statistics', i = 0):
+        with self.subTest('Test of statistics', i=0):
             actual_stats = actual_results['statistics']
             self.assertEqual(actual_stats['total_label_count'], 8)
             self.assertEqual(len(actual_stats['items_missing_label']), 1)
@@ -435,7 +529,7 @@ class TestValidateAnnotations(TestValidatorTemplate):
                 len(undefined_attr_dets['items_with_undefined_attr']), 1)
             self.assertEqual(undefined_attr_dets['distribution'], {'5': 1})
 
-        with self.subTest('Test of validation reports', i = 1):
+        with self.subTest('Test of validation reports', i=1):
             actual_reports = actual_results['validation_reports']
             report_types = [r['anomaly_type'] for r in actual_reports]
             report_count_by_type = Counter(report_types)
@@ -449,7 +543,7 @@ class TestValidateAnnotations(TestValidatorTemplate):
             self.assertEqual(report_count_by_type['OnlyOneAttributeValue'], 1)
             self.assertEqual(report_count_by_type['MissingAttribute'], 1)
 
-        with self.subTest('Test of summary', i = 2):
+        with self.subTest('Test of summary', i=2):
             actual_summary = actual_results['summary']
             expected_summary = {
                 'errors': 10,
@@ -461,7 +555,7 @@ class TestValidateAnnotations(TestValidatorTemplate):
     def test_validate_annotations_detection(self):
         actual_results = validate_annotations(self.dataset, 'detection')
 
-        with self.subTest('Test of statistics', i = 0):
+        with self.subTest('Test of statistics', i=0):
             actual_stats = actual_results['statistics']
             self.assertEqual(actual_stats['total_bbox_count'], 8)
             self.assertEqual(len(actual_stats['items_missing_bbox']), 1)
@@ -489,22 +583,24 @@ class TestValidateAnnotations(TestValidatorTemplate):
             bbox_dist_item = actual_stats['bbox_distribution_in_dataset_item']
             self.assertEqual(sum(bbox_dist_item.values()), 8)
 
-        with self.subTest('Test of validation reports', i = 1):
+        with self.subTest('Test of validation reports', i=1):
             actual_reports = actual_results['validation_reports']
             report_types = [r['anomaly_type'] for r in actual_reports]
-            report_count_by_type = Counter(report_types)
+            count_by_type = Counter(report_types)
 
-            self.assertEqual(len(actual_reports), 11)
-            self.assertEqual(report_count_by_type['FewSamplesInAttribute'], 4)
-            self.assertEqual(report_count_by_type['UndefinedAttribute'], 4)
-            self.assertEqual(report_count_by_type['UndefinedLabel'], 2)
-            self.assertEqual(report_count_by_type['MissingBboxAnnotation'], 1)
+            self.assertEqual(len(actual_reports), 45)
+            self.assertEqual(count_by_type['ImbalancedBboxDistInAttribute'], 32)
+            self.assertEqual(count_by_type['FewSamplesInAttribute'], 4)
+            self.assertEqual(count_by_type['UndefinedAttribute'], 4)
+            self.assertEqual(count_by_type['ImbalancedBboxDistInLabel'], 2)
+            self.assertEqual(count_by_type['UndefinedLabel'], 2)
+            self.assertEqual(count_by_type['MissingBboxAnnotation'], 1)
 
-        with self.subTest('Test of summary', i = 2):
+        with self.subTest('Test of summary', i=2):
             actual_summary = actual_results['summary']
             expected_summary = {
                 'errors': 6,
-                'warnings': 5
+                'warnings': 39
             }
 
             self.assertEqual(actual_summary, expected_summary)
