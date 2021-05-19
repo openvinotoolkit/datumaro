a02f9569b9eb390f77b738f9625837ae4649cf36
Slawomir Strehlke <slawomir.strehlke@intel.com>
2021-05-19 14:18:02 +0200

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 def pytest_configure(config):
     # register an additional markers
-    config.addinivalue_line("markers", "component": mark specific test type")
+    config.addinivalue_line("markers", "component: mark specific test type")
     config.addinivalue_line("markers", "priority_low: mark tests with priority low")
     config.addinivalue_line("markers", "priority_medium: mark specific test type")
     config.addinivalue_line("markers", "priority_high: mark specific test type")
