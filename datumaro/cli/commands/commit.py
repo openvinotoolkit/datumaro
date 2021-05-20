1ebb48a93042c587931d3a388047c820c305cac7
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-05-20 11:03:35 +0300

    Update commit command

--- datumaro/cli/commands/commit.py
+++ datumaro/cli/commands/commit.py
@@ -10,8 +10,6 @@ from ..util.project import load_project
 def build_parser(parser_ctor=argparse.ArgumentParser):
     parser = parser_ctor()
 
-    parser.add_argument('paths', nargs='*',
-        help="Files to include in the commit (default: all tracked)")
     parser.add_argument('-m', '--message', required=True, help="Commit message")
     parser.add_argument('-p', '--project', dest='project_dir', default='.',
         help="Directory of the project to operate on (default: current dir)")
@@ -22,6 +20,6 @@ def build_parser(parser_ctor=argparse.ArgumentParser):
 def commit_command(args):
     project = load_project(args.project_dir)
 
-    project.vcs.commit(args.paths, args.message)
+    project.commit(args.message)
 
     return 0
