96c4c4a7e82e3e2eb767e224deb5992f055636fc
Slawomir Strehlke <slawomir.strehlke@intel.com>
2021-05-24 17:16:02 +0200

    removed unneeded comments in requirements.py

--- tests/pytest_marking_constants/requirements.py
+++ tests/pytest_marking_constants/requirements.py
@@ -5,8 +5,5 @@
 
 class Requirements:
 
-    # Dummy requirement
-    DATUM_DUMMY_REQ = "Dummy requirement"
-
-    # [DATUMARO] Add SNYK scan integration
+    DATUM_GENERAL_REQ = "Datumaro general requirement"
     DATUM_244 = "DATUM-244 Add Snyk integration"
