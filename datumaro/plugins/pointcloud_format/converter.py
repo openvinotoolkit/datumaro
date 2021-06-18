9fe6fd07d6b9a3fca8e6e3a0a3660ad20f28ead4
cdp <cdp123>
2021-06-18 21:24:39 +0530

    Fix wrt CVAT changes

--- datumaro/plugins/pointcloud_format/converter.py
+++ datumaro/plugins/pointcloud_format/converter.py
@@ -117,44 +117,41 @@ class PointCloudParser:
                 break
 
     def set_attribute_data(self):
-        for data in self._annotation:
-            for item in data.annotations:
-                if item.label in self._labels:
+        labels = self._annotation.categories().get(AnnotationType.label, LabelCategories())
+        for label in labels._indices.values():
+
+            self._labels.append(label)
+            for attrs in self._get_label(label).attributes:
+                self._attribute_length += 1
+
+                tag_id = self._attribute_length
+                self.set_tags_key(tag_id)
+
+                if attrs == "occluded":
                     continue
 
-                self._labels.append(item.label)
-                for attrs in self._get_label(item.label).attributes:
-                    self._attribute_length += 1
+                tag = {
+                    "name": attrs,
+                    "value_type": "text",
+                    "color": "",
+                    "id": tag_id,
+                    "hotkey": "",
+                    "applicable_type": "imagesOnly",
+                    "classes": []
+                }
 
-                    tag_id = self._attribute_length
-                    self.set_tags_key(tag_id)
+                self._meta_data["tags"].append(tag)
 
-                    if attrs == "occluded":
-                        continue
+                tag = {
+                    "name": attrs,
+                    "value": self._get_label(label).name,
+                    "labelerLogin": self._user["name"],
+                    "createdAt": self._user["createdAt"],
+                    "updatedAt": self._user["updatedAt"],
+                    "key": self.get_tag_key(tag_id)
+                }
 
-                    tag = {
-                        "name": attrs,
-                        "value_type": "text",
-                        "color": "",
-                        "id": tag_id,
-                        "hotkey": "",
-                        "applicable_type": "imagesOnly",
-                        "classes": []
-                    }
-
-                    self._meta_data["tags"].append(tag)
-
-                    tag = {
-                        "name": attrs,
-                        "value": self._get_label(item.label).name,
-                        "labelerLogin": self._user["name"],
-                        "createdAt": self._user["createdAt"],
-                        "updatedAt": self._user["updatedAt"],
-                        "key": self.get_tag_key(tag_id)
-                    }
-
-                    self._tags.append(tag)
-
+                self._tags.append(tag)
 
     def set_label_data(self):
         classes_info = []
