3a291046ba9cf31cfabc0e633099a8cae3b593b9
Roman Donchenko <roman.donchenko@intel.com>
2021-06-24 15:41:30 +0300

    Fix whitespace errors
    As detected with `git diff --check`.

--- datumaro/components/__init__.py
+++ datumaro/components/__init__.py
@@ -2,4 +2,3 @@
 # Copyright (C) 2019-2020 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
-
--- datumaro/plugins/openvino_plugin/launcher.py
+++ datumaro/plugins/openvino_plugin/launcher.py
@@ -195,4 +195,3 @@ class OpenvinoLauncher(Launcher):
 
     def process_outputs(self, inputs, outputs):
         return self._interpreter.process_outputs(inputs, outputs)
-
