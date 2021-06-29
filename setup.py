4fe6b78a74dbaacb943719e2c06cb64450dcbf34
dvkruchinin <dvkruchinin@gmail.com>
2021-06-29 14:04:16 +0300

    Exclude "docs", "ci" folders from setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/openvinotoolkit/datumaro",
-    packages=setuptools.find_packages(exclude=['tests*']),
+    packages=setuptools.find_packages(exclude=['tests*', 'docs*', 'ci*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
