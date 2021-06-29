53d46c18989da4ac13abf45d125f2e7675508ffd
dvkruchinin <dvkruchinin@gmail.com>
2021-06-29 10:40:43 +0300

    Simple update

--- json_to_html.py
+++ json_to_html.py
@@ -12,10 +12,10 @@ from json2html import *
 def json_to_html(path_to_json):
     with open(path_to_json) as json_file:
         data = json.load(json_file)
-    hadolint_html_report = json2html.convert(json = data)
+    html_report = json2html.convert(json = data)
 
     with open(os.path.splitext(path_to_json)[0] + '.html', 'w') as html_file:
-        html_file.write(hadolint_html_report)
+        html_file.write(html_report)
 
 
 if __name__ == '__main__':
