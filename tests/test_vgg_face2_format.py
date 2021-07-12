7f52e1e64cceeb4fea61d3cfcfac6a2c2612bf1f
kirill.sizov <kirill.sizov@intel.com>
2021-07-12 10:04:47 +0300

    fix tests

--- tests/test_vgg_face2_format.py
+++ tests/test_vgg_face2_format.py
@@ -20,35 +20,35 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_and_load(self):
         source_dataset = Dataset.from_iterable([
-            DatasetItem(id='1', subset='train', image=np.ones((8, 8, 3)),
+            DatasetItem(id='label_0/1', subset='train', image=np.ones((8, 8, 3)),
                 annotations=[
                     Bbox(0, 2, 4, 2, label=0),
                     Points([3.2, 3.12, 4.11, 3.2, 2.11,
                         2.5, 3.5, 2.11, 3.8, 2.13], label=0),
                 ]
             ),
-            DatasetItem(id='2', subset='train', image=np.ones((10, 10, 3)),
+            DatasetItem(id='label_1/2', subset='train', image=np.ones((10, 10, 3)),
                 annotations=[
                     Points([4.23, 4.32, 5.34, 4.45, 3.54,
                         3.56, 4.52, 3.51, 4.78, 3.34], label=1),
                 ]
             ),
-            DatasetItem(id='3', subset='train', image=np.ones((8, 8, 3)),
+            DatasetItem(id='label_2/3', subset='train', image=np.ones((8, 8, 3)),
                 annotations=[Label(2)]
             ),
-            DatasetItem(id='4', subset='train', image=np.ones((10, 10, 3)),
+            DatasetItem(id='label_3/4', subset='train', image=np.ones((10, 10, 3)),
                 annotations=[
                     Bbox(0, 2, 4, 2, label=3),
                     Points([3.2, 3.12, 4.11, 3.2, 2.11,
                         2.5, 3.5, 2.11, 3.8, 2.13], label=3),
                 ]
             ),
-            DatasetItem(id='a/5', subset='train', image=np.ones((8, 8, 3)),
+            DatasetItem(id='no_label/a/5', subset='train', image=np.ones((8, 8, 3)),
                 annotations=[
                     Bbox(2, 2, 2, 2),
                 ]
             ),
-            DatasetItem(id='label_0', subset='train', image=np.ones((8, 8, 3)),
+            DatasetItem(id='no_label/label_0', subset='train', image=np.ones((8, 8, 3)),
             ),
         ], categories={
             AnnotationType.label: LabelCategories.from_iterable(
@@ -64,7 +64,7 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_dataset_with_no_subsets(self):
         source_dataset = Dataset.from_iterable([
-            DatasetItem(id='b/1', image=np.ones((8, 8, 3)),
+            DatasetItem(id='a/b/1', image=np.ones((8, 8, 3)),
                 annotations=[
                     Bbox(0, 2, 4, 2, label=0),
                     Points([4.23, 4.32, 5.34, 4.45, 3.54,
@@ -82,7 +82,7 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_dataset_with_cyrillic_and_spaces_in_filename(self):
         source_dataset = Dataset.from_iterable([
-            DatasetItem(id='кириллица с пробелом', image=np.ones((8, 8, 3)),
+            DatasetItem(id='a/кириллица с пробелом', image=np.ones((8, 8, 3)),
                 annotations=[
                     Points([4.23, 4.32, 5.34, 4.45, 3.54,
                         3.56, 4.52, 3.51, 4.78, 3.34], label=0),
@@ -100,7 +100,7 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_dataset_with_no_save_images(self):
         source_dataset = Dataset.from_iterable([
-            DatasetItem(id='1', image=np.ones((8, 8, 3)),
+            DatasetItem(id='label_0/1', image=np.ones((8, 8, 3)),
                 annotations=[
                     Bbox(0, 2, 4, 2, label=0),
                     Points([4.23, 4.32, 5.34, 4.45, 3.54,
@@ -118,14 +118,14 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_dataset_with_no_labels(self):
         source_dataset = Dataset.from_iterable([
-            DatasetItem(id='1', image=np.ones((8, 8, 3)),
+            DatasetItem(id='no_label/1', image=np.ones((8, 8, 3)),
                 annotations=[
                     Bbox(0, 2, 4, 2),
                     Points([4.23, 4.32, 5.34, 4.45, 3.54,
                         3.56, 4.52, 3.51, 4.78, 3.34]),
                 ]
             ),
-            DatasetItem(id='2', image=np.ones((8, 8, 3)),
+            DatasetItem(id='no_label/2', image=np.ones((8, 8, 3)),
                 annotations=[
                     Bbox(2, 2, 4, 2),
                 ]
@@ -141,7 +141,7 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_dataset_with_wrong_number_of_points(self):
         source_dataset = Dataset.from_iterable([
-            DatasetItem(id='1', image=np.ones((8, 8, 3)),
+            DatasetItem(id='no_label/1', image=np.ones((8, 8, 3)),
                 annotations=[
                     Points([4.23, 4.32, 5.34, 3.51, 4.78, 3.34]),
                 ]
@@ -149,7 +149,7 @@ class VggFace2FormatTest(TestCase):
         ], categories=[])
 
         target_dataset = Dataset.from_iterable([
-           DatasetItem(id='1', image=np.ones((8, 8, 3)),
+           DatasetItem(id='no_label/1', image=np.ones((8, 8, 3)),
                 annotations=[]
             ),
         ], categories=[])
@@ -163,7 +163,7 @@ class VggFace2FormatTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_save_and_load_image_with_arbitrary_extension(self):
         dataset = Dataset.from_iterable([
-            DatasetItem('q/1', image=Image(path='q/1.JPEG',
+            DatasetItem('no_label/q/1', image=Image(path='q/1.JPEG',
                 data=np.zeros((4, 3, 3)))),
             DatasetItem('a/b/c/2', image=Image(path='a/b/c/2.bmp',
                     data=np.zeros((3, 4, 3))),
@@ -191,7 +191,7 @@ class VggFace2ImporterTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_import(self):
         expected_dataset = Dataset.from_iterable([
-            DatasetItem(id='0001_01', subset='train',
+            DatasetItem(id='n000001/0001_01', subset='train',
                 image=np.ones((10, 15, 3)),
                 annotations=[
                     Bbox(2, 2, 1, 2, label=0),
@@ -199,7 +199,15 @@ class VggFace2ImporterTest(TestCase):
                         2.456, 2.81, 2.32, 2.89, 2.3], label=0),
                 ]
             ),
-            DatasetItem(id='0002_01', subset='train',
+            DatasetItem(id='n000002/0001_01', subset='train',
+                image=np.ones((10, 15, 3)),
+                annotations=[
+                    Bbox(2, 4, 2, 2, label=1),
+                    Points([2.3, 4.9, 2.9, 4.93, 2.62,
+                        4.745, 2.54, 4.45, 2.76, 4.43], label=1)
+                ]
+            ),
+            DatasetItem(id='n000002/0002_01', subset='train',
                 image=np.ones((10, 15, 3)),
                 annotations=[
                     Bbox(1, 3, 1, 1, label=1),
