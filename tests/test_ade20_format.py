9f63add0a33391596634753565db916e10a680d1
kirill.sizov <kirill.sizov@intel.com>
2021-08-05 06:56:36 +0300

    Add test

---
+++ tests/test_ade20_format.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2021 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+from unittest.case import TestCase
+import os.path as osp
+import numpy as np
+
+from datumaro.components.dataset import DatasetItem, Dataset
+from datumaro.components.extractor import AnnotationType, LabelCategories, Mask
+from datumaro.util.test_utils import compare_datasets
+
+from tests.requirements import Requirements, mark_requirement
+
+DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'ade20_dataset')
+
+class OpenImagesImporterTest(TestCase):
+    @mark_requirement(Requirements.DATUM_399)
+    def test_can_import(self):
+        expected_dataset = Dataset.from_iterable(
+            [
+                DatasetItem(id='1', subset='training',
+                    image=np.ones((3, 4, 3)),
+                    annotations=[
+                        Mask(image=np.array([[0, 1, 0, 0]] * 3), label=0,
+                            group=1, attributes={'part_level': 0}),
+                        Mask(image=np.array([[0, 0, 0, 1]] * 3), label=2,
+                            group=1, attributes={'part_level': 1}),
+                        Mask(image=np.array([[0, 0, 1, 1]] * 3),
+                            group=2, label=1,
+                            attributes={'walkin': True, 'part_level': 0})
+                    ]),
+                DatasetItem(id='2', subset='validation',
+                    image=np.ones((3, 4, 3)),
+                    annotations=[
+                        Mask(image=np.array([[0, 1, 0, 1]] * 3), label=0,
+                            group=1, attributes={'part_level': 0}),
+                        Mask(image=np.array([[0, 0, 1, 0]] * 3), label=1,
+                            group=2, attributes={'part_level': 0}),
+                        Mask(image=np.array([[0, 1, 0, 1]] * 3), label=2,
+                            group=1, attributes={'part_level': 1}),
+                        Mask(image=np.array([[0, 1, 0, 0]] * 3), label=3,
+                            group=1, attributes={'part_level': 2})
+                    ])
+            ], categories={AnnotationType.label: LabelCategories.from_iterable([
+                    ('sky', 'street'), ('person', 'street'),
+                    ('license plate', 'street'), 'rim', 'street'])
+                }
+        )
+
+        dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'ade20')
+        compare_datasets(self, dataset, expected_dataset)
