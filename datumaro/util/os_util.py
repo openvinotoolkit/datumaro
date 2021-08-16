ceda7b8e7fb37b267c8d6689b2a6d4a0449c0a7c
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-08-16 15:55:36 +0300

    Make git optional

--- datumaro/util/os_util.py
+++ datumaro/util/os_util.py
@@ -15,12 +15,18 @@ import subprocess
 import sys
 import unicodedata
 
-# Declare functions to remove files and directories.
-#
-# Use rmtree from GitPython to avoid the problem with removal of
-# readonly files on Windows, which Git uses extensively
-# It double checks if a file cannot be removed because of readonly flag
-from git.util import rmfile, rmtree  # pylint: disable=unused-import
+try:
+    import git
+
+    # Declare functions to remove files and directories.
+    #
+    # Use rmtree from GitPython to avoid the problem with removal of
+    # readonly files on Windows, which Git uses extensively
+    # It double checks if a file cannot be removed because of readonly flag
+    from git.util import rmfile, rmtree  # pylint: disable=unused-import
+except ImportError:
+    from os import remove as rmfile # pylint: disable=unused-import
+    from shutil import rmtree as rmtree # pylint: disable=unused-import
 
 from . import cast
 
