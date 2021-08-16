08f04a4bcb1c4d05891320c4918adfc6d7d8c8c4
kirill.sizov <kirill.sizov@intel.com>
2021-08-16 11:04:25 +0300

    Update tests

--- tests/test_ade20k2017_format.py
+++ tests/test_ade20k2017_format.py
@@ -13,9 +13,9 @@ from datumaro.util.test_utils import compare_datasets
 
 from tests.requirements import Requirements, mark_requirement
 
-DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'ade20k_dataset')
+DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'ade20k2017_dataset')
 
-class Ade20kImporterTest(TestCase):
+class Ade20k2017ImporterTest(TestCase):
     @mark_requirement(Requirements.DATUM_399)
     def test_can_import(self):
         expected_dataset = Dataset.from_iterable(
@@ -23,31 +23,31 @@ class Ade20kImporterTest(TestCase):
                 DatasetItem(id='1', subset='training',
                     image=np.ones((3, 4, 3)),
                     annotations=[
-                        Mask(image=np.array([[0, 1, 0, 0]] * 3), label=1,
+                        Mask(image=np.array([[0, 1, 0, 0]] * 3), label=0,
                             group=1, attributes={'part_level': 0}, z_order=0),
-                        Mask(image=np.array([[0, 0, 0, 1]] * 3), label=3,
+                        Mask(image=np.array([[0, 0, 0, 1]] * 3), label=2,
                             group=1, attributes={'part_level': 1}, z_order=1),
                         Mask(image=np.array([[0, 0, 1, 1]] * 3),
-                            group=2, label=2, z_order=0,
+                            group=2, label=1, z_order=0,
                             attributes={'walkin': True, 'part_level': 0})
                     ]),
                 DatasetItem(id='2', subset='validation',
                     image=np.ones((3, 4, 3)),
                     annotations=[
-                        Mask(image=np.array([[0, 1, 0, 1]] * 3), label=1,
+                        Mask(image=np.array([[0, 1, 0, 1]] * 3), label=0,
                             z_order=0, group=1, attributes={'part_level': 0}),
-                        Mask(image=np.array([[0, 0, 1, 0]] * 3), label=2,
+                        Mask(image=np.array([[0, 0, 1, 0]] * 3), label=1,
                             z_order=0, group=2, attributes={'part_level': 0}),
-                        Mask(image=np.array([[0, 1, 0, 1]] * 3), label=3,
+                        Mask(image=np.array([[0, 1, 0, 1]] * 3), label=2,
                             z_order=1, group=1, attributes={'part_level': 1}),
-                        Mask(image=np.array([[0, 1, 0, 0]] * 3), label=4,
+                        Mask(image=np.array([[0, 1, 0, 0]] * 3), label=3,
                             z_order=2, group=1, attributes={'part_level': 2})
                     ])
             ], categories={AnnotationType.label: LabelCategories.from_iterable([
-                    'street', ('sky', 'street'), ('person', 'street'),
-                    ('license plate', 'street'), 'rim'])
+                    'sky', 'person', 'license plate', 'rim'])
                 }
         )
 
-        imported_dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'ade20k')
-        compare_datasets(self, expected_dataset, imported_dataset, require_images=True)
+        imported_dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'ade20k2017')
+        compare_datasets(self, expected_dataset, imported_dataset,
+            require_images=True)
---
+++ tests/test_ade20k2020_format.py
@@ -0,0 +1,68 @@
+# Copyright (C) 2021 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from unittest.case import TestCase
+import os.path as osp
+
+import numpy as np
+
+from datumaro.components.dataset import Dataset, DatasetItem
+from datumaro.components.extractor import AnnotationType, LabelCategories, Mask
+from datumaro.util.test_utils import compare_datasets
+
+from tests.requirements import Requirements, mark_requirement
+
+DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'ade20k2020_dataset')
+
+class Ade20k2020ImporterTest(TestCase):
+    @mark_requirement(Requirements.DATUM_399)
+    def test_can_import(self):
+        expected_dataset = Dataset.from_iterable(
+            [
+                DatasetItem(id='1', subset='training',
+                    image=np.ones((5, 5, 3)),
+                    annotations=[
+                        Mask(image=np.array([[0, 0, 1, 1, 1]] * 5), label=0,
+                            group=401, z_order=0, id=401),
+                        Mask(image=np.array([[0, 1, 0, 0, 0]] * 5), label=1,
+                            group=1831, z_order=0, id=1831),
+                        Mask(image=np.array([[0, 0, 0, 1, 1]] * 5), label=2,
+                            id=774, group=774, z_order=1),
+                        Mask(image=np.array([[0, 0, 1, 1, 1]] * 5), label=0,
+                            group=0, z_order=0, attributes={'part_level': 0}),
+                        Mask(image=np.array([[0, 1, 0, 0, 0]] * 5), label=1,
+                            group=1, z_order=0,
+                            attributes={'walkin': True, 'part_level': 0}),
+                        Mask(image=np.array([[0, 0, 0, 1, 1]] * 5), label=2,
+                            group=2, z_order=1, attributes={'part_level': 1}),
+                    ]),
+                DatasetItem(id='2', subset='validation',
+                    image=np.ones((5, 5, 3)),
+                    annotations=[
+                        Mask(image=np.array([[0, 0, 1, 1, 1]] * 5), label=0,
+                            group=401, z_order=0, id=401),
+                        Mask(image=np.array([[0, 1, 0, 0, 0]] * 5), label=1,
+                            group=1831, z_order=0, id=1831),
+                        Mask(image=np.array([[0, 0, 0, 0, 1]] * 5), label=3,
+                            group=2122, z_order=2, id=2122),
+                        Mask(image=np.array([[0, 0, 0, 1, 1]] * 5), label=2,
+                            group=774, z_order=1, id=774),
+                        Mask(image=np.array([[0, 0, 1, 1, 1]] * 5), label=0,
+                            group=0, z_order=0, attributes={'part_level': 0}),
+                        Mask(image=np.array([[0, 1, 0, 0, 0]] * 5), label=1,
+                            group=1, z_order=0,
+                            attributes={'walkin': True, 'part_level': 0}),
+                        Mask(image=np.array([[0, 0, 0, 1, 1]] * 5), label=2,
+                            group=2, z_order=1, attributes={'part_level': 1}),
+                        Mask(image=np.array([[0, 0, 0, 0, 1]] * 5), label=3,
+                            group=3, z_order=2, attributes={'part_level': 2}),
+                    ])
+            ], categories={AnnotationType.label: LabelCategories.from_iterable([
+                    'car', 'person', 'door', 'rim'])
+                }
+        )
+
+        imported_dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'ade20k2020')
+        compare_datasets(self, expected_dataset, imported_dataset,
+            require_images=True)
