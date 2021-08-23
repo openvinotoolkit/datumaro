596b55aced14ed303aebe5863e2d5c07f61eeae6
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-08-23 15:03:17 +0300

    Remove extra line

--- tests/test_coco_format.py
+++ tests/test_coco_format.py
@@ -1057,4 +1057,3 @@ class CocoConverterTest(TestCase):
             self._test_save_and_load(source_dataset,
                  partial(CocoInstancesConverter.convert),
                  test_dir, target_dataset=target_dataset)
-
