e8eed12e4b486eb85418f236de2f96d320bf762c
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-09-07 22:24:27 +0300

    Update datumaro/plugins/widerface_format.py

--- datumaro/plugins/widerface_format.py
+++ datumaro/plugins/widerface_format.py
@@ -34,7 +34,7 @@ class WiderFaceExtractor(SourceExtractor):
 
         if not subset:
             subset = osp.splitext(osp.basename(path))[0]
-            if re.fullmatch(r'wider_face_\S+(_bbx_gt)|(_filelist)', subset):
+            if re.fullmatch(r'wider_face_\S+((_bbx_gt)|(_filelist))', subset):
                 subset = subset.split('_')[2]
         super().__init__(subset=subset)
 
