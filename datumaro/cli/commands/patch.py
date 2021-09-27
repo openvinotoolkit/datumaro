38feea859051c12b4357182425242a1dce65f7b9
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-09-27 17:03:27 +0300

    Update patch command

--- datumaro/cli/commands/patch.py
+++ datumaro/cli/commands/patch.py
@@ -7,7 +7,9 @@ import logging as log
 import os
 import os.path as osp
 
+from datumaro.components.environment import Environment
 from datumaro.components.errors import ProjectNotFoundError
+from datumaro.util.scope import scope_add, scoped
 
 from ..util import MultilineFormatter
 from ..util.errors import CliException
@@ -18,15 +20,32 @@ def build_parser(parser_ctor=argparse.ArgumentParser):
     parser = parser_ctor(help="Updates dataset from another one",
         description="""
         Updates items of the first dataset with items from the second one.
-        By default, this command changes datasets in-place, but there is an
-        option to specify output directory.|n
+        By default, datasets are updated in-place. The '-o/--output-dir'
+        option can be used to specify another output directory. When
+        updating inplace, use the '--overwrite' parameter along with the
+        '--save-images' export option (inplace updates fail by default
+        to prevent data loss).|n
+        |n
+        Unlike the regular project data source joining, the datasets are not
+        required to have the same labels. The labels from the "patch"
+        dataset are projected onto the labels of the patched dataset,
+        so only the annotations with the matching labels are used, i.e.
+        all the annotations having unknown labels are ignored. Currently,
+        this command doesn't allow to update the label information in the
+        patched dataset.|n
+        |n
+        The command supports passing extra exporting options for the output
+        dataset. The extra options should be passed after the main arguments
+        and after the '--' separator. Particularly, this is useful to include
+        images in the output dataset with '--save-images'.|n
+        |n
         This command can be applied to the current project targets or
-        arbitrary datasets outside the project. Note that if the destination
+        arbitrary datasets outside a project. Note that if the destination
         is read-only (e.g. if it is a project, stage or a cache entry),
         the output directory must be provided.|n
         |n
         This command has the following invocation syntax:
-        - %(prog)s <destination revpath> <patch revpath>|n
+        - %(prog)s <target dataset revpath> <patch dataset revpath>|n
         |n
         <revpath> - either a dataset path or a revision path. The full
         syntax is:|n
@@ -37,63 +56,87 @@ def build_parser(parser_ctor=argparse.ArgumentParser):
         |s|s- <rev> [ :<target> ]|n
         |s|s- <target>|n
         |n
-        The current project (-p/--project) is used as a context for plugins.
-        It can be useful for dataset paths in targets. When not specified,
-        the current project's working tree is used.|n
+        The current project (-p/--project) is also used as a context for
+        plugins, so it can be useful for dataset paths having custom formats.
+        When not specified, the current project's working tree is used.|n
         |n
         Examples:|n
         - Update a VOC-like dataset with a COCO-like annotations:|n
-        |s|s%(prog)s path/to/dataset1:voc path/to/dataset2:coco|n
+        |s|s%(prog)s --overwrite dataset1/:voc dataset2/:coco -- --save-images|n
         |n
         - Generate a patched dataset, based on a project:|n
         |s|s%(prog)s -o patched_proj1/ proj1/ proj2/|n
         |n
         - Update the "source1" the current project with a dataset:|n
-        |s|s%(prog)s source1 path/to/dataset2:coco|n
+        |s|s%(prog)s -p proj/ --overwrite source1 path/to/dataset2:coco|n
         |n
         - Generate a patched source from a previous revision and a dataset:|n
-        |s|s%(prog)s -o new_src2/ HEAD~2:source-2 path/to/dataset2:yolo
+        |s|s%(prog)s -o new_src2/ HEAD~2:source-2 path/to/dataset2:yolo|n
+        |n
+        - Update a dataset in a custom format, described in a project plugin:|n
+        |s|s%(prog)s -p proj/ --overwrite dataset/:my_format dataset2/:coco|n
+        |n
         """,
         formatter_class=MultilineFormatter)
 
-    parser.add_argument('destination', help="Target dataset revpath")
+    parser.add_argument('target', help="Target dataset revpath")
     parser.add_argument('patch', help="Patch dataset revpath")
     parser.add_argument('-o', '--output-dir', dest='dst_dir', default=None,
         help="Output directory (default: save in-place)")
     parser.add_argument('--overwrite', action='store_true',
         help="Overwrite existing files in the save directory, "
-            "if it is specified and not empty")
+            "if it is not empty")
     parser.add_argument('-p', '--project', dest='project_dir',
         help="Directory of the 'current' project (default: current dir)")
+    parser.add_argument('extra_args', nargs=argparse.REMAINDER,
+        help="Additional arguments for exporting (pass '-- -h' for help). "
+            "Must be specified after the main command arguments and after "
+            "the '--' separator")
     parser.set_defaults(command=patch_command)
 
     return parser
 
+@scoped
 def patch_command(args):
-    dst_dir = args.dst_dir
-    if dst_dir:
-        if not args.overwrite and osp.isdir(dst_dir) and os.listdir(dst_dir):
-            raise CliException("Directory '%s' already exists "
-                "(pass --overwrite to overwrite)" % dst_dir)
-        dst_dir = osp.abspath(dst_dir)
-
     project = None
     try:
-        project = load_project(args.project_dir)
+        project = scope_add(load_project(args.project_dir))
     except ProjectNotFoundError:
         if args.project_dir:
             raise
 
-    destination_dataset = parse_full_revpath(args.destination, project)
-    patch_dataset = parse_full_revpath(args.patch, project)
+    if project is not None:
+        env = project.env
+    else:
+        env = Environment()
 
-    destination_dataset.update(patch_dataset)
+    target_dataset, _project = parse_full_revpath(args.target, project)
+    if _project is not None:
+        scope_add(_project)
 
-    # TODO: Probably, a better way to control export
-    # options is needed. Maybe, just allow converter
-    # parameters like in export.
-    destination_dataset.save(save_dir=dst_dir,
-        save_images=True) # avoid spontaneous removal of images
+    try:
+        converter = env.converters[target_dataset.format]
+    except KeyError:
+        raise CliException("Converter for format '%s' is not found" % \
+            args.format)
+
+    extra_args = converter.parse_cmdline(args.extra_args)
+
+    dst_dir = args.dst_dir
+    if not dst_dir:
+        dst_dir = target_dataset.data_path
+    if not args.overwrite and osp.isdir(dst_dir) and os.listdir(dst_dir):
+        raise CliException("Directory '%s' already exists "
+            "(pass --overwrite to overwrite)" % dst_dir)
+    dst_dir = osp.abspath(dst_dir)
+
+    patch_dataset, _project = parse_full_revpath(args.patch, project)
+    if _project is not None:
+        scope_add(_project)
+
+    target_dataset.update(patch_dataset)
+
+    target_dataset.save(save_dir=dst_dir, **extra_args)
 
     log.info("Patched dataset has been saved to '%s'" % dst_dir)
 
---
+++ tests/cli/test_patch.py
@@ -0,0 +1,133 @@
+from unittest import TestCase
+import os.path as osp
+
+import numpy as np
+
+from datumaro.components.annotation import Bbox
+from datumaro.components.errors import ReadonlyDatasetError
+from datumaro.components.extractor import DatasetItem
+from datumaro.components.project import Dataset, Project
+from datumaro.util.test_utils import TestDir, compare_datasets
+from datumaro.util.test_utils import run_datum as run
+
+from ..requirements import Requirements, mark_requirement
+
+
+class MergeTest(TestCase):
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_can_run_patch(self):
+        dataset = Dataset.from_iterable([
+            # Must be overridden
+            DatasetItem(id=100, subset='train', image=np.ones((10, 6, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 3, label=0),
+                ]),
+
+            # Must be kept
+            DatasetItem(id=1, image=np.ones((5, 4, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 4, label=1)
+                ]),
+        ], categories=['a', 'b'])
+
+        patch = Dataset.from_iterable([
+            # Must override
+            DatasetItem(id=100, subset='train', image=np.ones((10, 6, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 4, label=0), # Label must be remapped
+                    Bbox(5, 6, 2, 3, label=1), # Label must be remapped
+                    Bbox(2, 2, 2, 3, label=2), # Will be dropped due to label
+                ]),
+
+            # Must be added
+            DatasetItem(id=2, image=np.ones((5, 4, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 2, label=1) # Label must be remapped
+                ]),
+        ], categories=['b', 'a', 'c'])
+
+        expected =  Dataset.from_iterable([
+            DatasetItem(id=100, subset='train', image=np.ones((10, 6, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 4, label=1, id=1, group=1),
+                    Bbox(5, 6, 2, 3, label=0, id=2, group=2),
+                ]),
+
+            DatasetItem(id=1, image=np.ones((5, 4, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 4, label=1, id=1, group=1)
+                ]),
+
+            DatasetItem(id=2, image=np.ones((5, 4, 3)),
+                annotations=[
+                    Bbox(1, 2, 3, 2, label=0, id=2, group=2)
+                ]),
+        ], categories=['a', 'b'])
+
+        with TestDir() as test_dir:
+            dataset_url = osp.join(test_dir, 'dataset1')
+            patch_url = osp.join(test_dir, 'dataset2')
+
+            dataset.export(dataset_url, 'coco', save_images=True)
+            patch.export(patch_url, 'voc', save_images=True)
+
+            run(self, 'patch', '--overwrite',
+                dataset_url + ':coco', patch_url + ':voc',
+                '--', '--reindex=1', '--save-images')
+
+            compare_datasets(self, expected,
+                Dataset.import_from(dataset_url, format='coco'),
+                require_images=True, ignored_attrs='*')
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_patch_fails_on_inplace_update_without_overwrite(self):
+        dataset1 = Dataset.from_iterable([
+            DatasetItem(id=1, annotations=[ Bbox(1, 2, 3, 4, label=1) ]),
+        ], categories=['a', 'b'])
+
+        dataset2 = Dataset.from_iterable([
+            DatasetItem(id=2, annotations=[ Bbox(1, 2, 3, 2, label=1) ]),
+        ], categories=['b', 'a', 'c'])
+
+        with TestDir() as test_dir:
+            dataset_url = osp.join(test_dir, 'dataset1')
+            patch_url = osp.join(test_dir, 'dataset2')
+
+            dataset1.export(dataset_url, 'coco', save_images=True)
+            dataset2.export(patch_url, 'coco', save_images=True)
+
+            run(self, 'patch', dataset_url + ':coco', patch_url + ':coco',
+                expected_code=1)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_patch_fails_on_inplace_update_of_stage(self):
+        dataset = Dataset.from_iterable([
+            DatasetItem(id=1, annotations=[ Bbox(1, 2, 3, 4, label=1) ]),
+        ], categories=['a', 'b'])
+
+        patch = Dataset.from_iterable([
+            DatasetItem(id=2, annotations=[ Bbox(1, 2, 3, 2, label=1) ]),
+        ], categories=['b', 'a', 'c'])
+
+        with TestDir() as test_dir:
+            dataset_url = osp.join(test_dir, 'dataset1')
+            patch_url = osp.join(test_dir, 'dataset2')
+
+            dataset.export(dataset_url, 'coco', save_images=True)
+            patch.export(patch_url, 'coco', save_images=True)
+
+            project_dir = osp.join(test_dir, 'proj')
+            with Project.init(project_dir) as project:
+                project.import_source('source-1', dataset_url, 'coco',
+                    no_cache=True)
+                project.commit('first commit')
+
+            with self.subTest('without overwrite'):
+                run(self, 'patch', '-p', project_dir,
+                    'HEAD:source-1', patch_url + ':coco',
+                    expected_code=1)
+
+            with self.subTest('with overwrite'):
+                with self.assertRaises(ReadonlyDatasetError):
+                    run(self, 'patch', '-p', project_dir, '--overwrite',
+                        'HEAD:source-1', patch_url + ':coco')
