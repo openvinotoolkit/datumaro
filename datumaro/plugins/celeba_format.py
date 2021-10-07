12be5ffe38389ac454d1def8d7ce1ca8c92681b4
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-10-07 19:59:04 +0300

    Some fixes

--- datumaro/plugins/celeba_format.py
+++ datumaro/plugins/celeba_format.py
@@ -7,6 +7,7 @@ import os.path as osp
 from datumaro.components.annotation import (
     AnnotationType, Bbox, Label, LabelCategories, Points,
 )
+from datumaro.components.errors import DatasetImportError
 from datumaro.components.extractor import DatasetItem, Importer, SourceExtractor
 from datumaro.util.image import find_images
 
@@ -26,7 +27,7 @@ class CelebaPath:
         'nose_x nose_y leftmouth_x leftmouth_y rightmouth_x rightmouth_y'
 
 class CelebaExtractor(SourceExtractor):
-    def __init__(self, path, subset=None):
+    def __init__(self, path):
         if not osp.isfile(path):
             raise FileNotFoundError("Can't read annotation file '%s'" % path)
 
@@ -39,19 +40,22 @@ class CelebaExtractor(SourceExtractor):
     def _load_items(self, path):
         items = {}
 
-        align_dataset = False
+        is_aligned = False
 
-        image_dir = osp.join(osp.dirname(self._anno_dir), CelebaPath.IMAGES_DIR)
+        image_dir = osp.join(osp.dirname(self._anno_dir),
+            CelebaPath.IMAGES_DIR)
         if not osp.isdir(image_dir):
-            image_dir = osp.join(osp.dirname(self._anno_dir), CelebaPath.IMAGES_ALIGN_DIR)
-            align_dataset = True
+            image_dir = osp.join(osp.dirname(self._anno_dir),
+                CelebaPath.IMAGES_ALIGN_DIR)
+            is_aligned = True
+
         if osp.isdir(image_dir):
             images = {
                 osp.splitext(osp.relpath(p, image_dir))[0].replace('\\', '/'): p
                 for p in find_images(image_dir, recursive=True)
             }
         else:
-            align_dataset = False
+            is_aligned = False
             images = {}
 
         label_categories = self._categories[AnnotationType.label]
@@ -61,37 +65,43 @@ class CelebaExtractor(SourceExtractor):
                 label_ids = [int(id) for id in item_ann]
                 anno = []
                 for label in label_ids:
-                    while label >= len(label_categories):
+                    while len(label_categories) <= label:
                         label_categories.add('class-%d' % len(label_categories))
                     anno.append(Label(label))
 
-                items[item_id] = DatasetItem(id=item_id, image=images.get(item_id),
-                    annotations=anno)
+                items[item_id] = DatasetItem(id=item_id,
+                    image=images.get(item_id), annotations=anno)
 
-        if not align_dataset:
+        if not is_aligned:
             bbox_path = osp.join(self._anno_dir, CelebaPath.BBOXES_FILE)
             if osp.isfile(bbox_path):
                 with open(bbox_path, encoding='utf-8') as f:
                     bboxes_number = int(f.readline().strip())
-                    counter = 0
+
                     if f.readline().strip() != CelebaPath.BBOXES_HEADER:
-                        raise Exception("File %s does not match "
-                            "the expected format '%s'" % bbox_path,
-                            CelebaPath.BBOXES_HEADER)
-                    for line in f:
+                        raise DatasetImportError("File '%s': the header "
+                            "does not match the expected format '%s'" % \
+                            (bbox_path, CelebaPath.BBOXES_HEADER))
+
+                    counter = 0
+                    for counter, line in enumerate(f):
                         item_id, item_ann = self.split_annotation(line)
                         bbox = [float(id) for id in item_ann]
+
                         if item_id not in items:
                             items[item_id] = DatasetItem(id=item_id,
                                 image=images.get(item_id))
+
                         anno = items[item_id].annotations
                         label = None
-                        if 0 < len(anno):
+                        if anno:
                             label = anno[0].label
-                        anno.append(Bbox(bbox[0], bbox[1], bbox[2], bbox[3], label=label))
-                        counter += 1
+                        anno.append(Bbox(
+                            bbox[0], bbox[1], bbox[2], bbox[3], label=label))
+
                     if bboxes_number != counter:
-                        raise Exception("File %s: the number of bounding "
+                        raise DatasetImportError(
+                            "File '%s': the number of bounding "
                             "boxes does not match the specified number "
                             "at the beginning of the file " % bbox_path)
 
@@ -99,66 +109,84 @@ class CelebaExtractor(SourceExtractor):
         if osp.isfile(attr_path):
             with open(attr_path, encoding='utf-8') as f:
                 attr_number = int(f.readline().strip())
-                counter = 0
                 attr_names = f.readline().split()
-                attr = {}
-                for line in f:
+
+                counter = 0
+                for counter, line in enumerate(f):
                     item_id, item_ann = self.split_annotation(line)
                     if len(attr_names) != len(item_ann):
-                        raise Exception("Line %s: the number of attributes "
+                        raise DatasetImportError("File '%s', line %s: "
+                            "the number of attributes "
                             "in the line does not match the number at the "
-                            "beginning of the file " % line)
-                    attr = {name: int(ann) > 0 for name, ann in zip(attr_names, item_ann)}
+                            "beginning of the file " % (attr_path, line))
+
+                    attrs = {name: 0 < int(ann)
+                        for name, ann in zip(attr_names, item_ann)}
 
                     if item_id not in items:
-                        items[item_id] = DatasetItem(id=item_id, image=images.get(item_id))
-                    items[item_id].attributes = attr
-                    counter += 1
+                        items[item_id] = DatasetItem(id=item_id,
+                            image=images.get(item_id))
+
+                    items[item_id].attributes = attrs
+
                 if attr_number != counter:
-                    raise Exception("File %s: the number of items with "
-                        "attributes does not match the specified number "
+                    raise DatasetImportError("File %s: the number of items "
+                        "with attributes does not match the specified number "
                         "at the beginning of the file " % attr_path)
-        if align_dataset:
-            landmark_path = osp.join(self._anno_dir, CelebaPath.LANDMARKS_ALIGN_FILE)
+
+        if is_aligned:
+            landmark_path = osp.join(self._anno_dir,
+                CelebaPath.LANDMARKS_ALIGN_FILE)
         else:
-            landmark_path = osp.join(self._anno_dir, CelebaPath.LANDMARKS_FILE)
+            landmark_path = osp.join(self._anno_dir,
+                CelebaPath.LANDMARKS_FILE)
         if osp.isfile(landmark_path):
             with open(landmark_path, encoding='utf-8') as f:
                 landmarks_number = int(f.readline().strip())
-                counter = 0
+
                 if f.readline().strip() != CelebaPath.LANDMARKS_HEADER:
-                    raise Exception("File %s does not match "
-                        "the expected format '%s'" % bbox_path,
-                        CelebaPath.LANDMARKS_HEADER)
-                for line in f:
+                    raise DatasetImportError("File '%s': the header "
+                        "does not match the expected format '%s'" % \
+                        (bbox_path, CelebaPath.LANDMARKS_HEADER))
+
+                counter = 0
+                for counter, line in enumerate(f):
                     item_id, item_ann = self.split_annotation(line)
                     landmarks = [float(id) for id in item_ann]
+
                     if item_id not in items:
-                        items[item_id] = DatasetItem(id=item_id, image=images.get(item_id))
+                        items[item_id] = DatasetItem(id=item_id,
+                            image=images.get(item_id))
+
                     anno = items[item_id].annotations
                     label = None
-                    if 0 < len(anno):
+                    if anno:
                         label = anno[0].label
                     anno.append(Points(landmarks, label=label))
-                    counter += 1
-                if landmarks_number != counter:
-                    raise Exception("File %s: the number of landmarks "
-                        "does not match the specified number at the "
-                        "beginning of the file " % landmark_path)
 
-        subset_path = osp.join(osp.dirname(self._anno_dir), CelebaPath.SUBSETS_FILE)
+                if landmarks_number != counter:
+                    raise DatasetImportError("File '%s': the number of "
+                        "landmarks does not match the specified number "
+                        "at the beginning of the file " % landmark_path)
+
+        subset_path = osp.join(osp.dirname(self._anno_dir),
+            CelebaPath.SUBSETS_FILE)
         if osp.isfile(subset_path):
             with open(subset_path, encoding='utf-8') as f:
                 for line in f:
                     item_id, item_ann = self.split_annotation(line)
-                    subset = item_ann[0]
+                    subset_id = item_ann[0]
+                    subset = CelebaPath.SUBSETS[subset_id]
+
                     if item_id not in items:
-                        items[item_id] = DatasetItem(id=item_id, image=images.get(item_id))
-                    items[item_id].subset = CelebaPath.SUBSETS[subset]
+                        items[item_id] = DatasetItem(id=item_id,
+                            image=images.get(item_id))
+
+                    items[item_id].subset = subset
+
                     if 'default' in self._subsets:
                         self._subsets.pop()
-                    if CelebaPath.SUBSETS[subset] not in self._subsets:
-                        self._subsets.append(CelebaPath.SUBSETS[subset])
+                    self._subsets.add(subset)
 
         return items
 
@@ -169,8 +197,8 @@ class CelebaExtractor(SourceExtractor):
                 item_id = osp.splitext(item[1])[0]
                 item = item[2].split()
             else:
-                raise Exception("Line %s: unexpected number "
-                     "of quotes in filename" % line)
+                raise DatasetImportError("Line %s: unexpected number "
+                    "of quotes in filename" % line)
         else:
             item = line.split()
             item_id = osp.splitext(item[0])[0]
--- tests/test_celeba_format.py
+++ tests/test_celeba_format.py
@@ -22,39 +22,54 @@ class CelebaImporterTest(TestCase):
         expected_dataset = Dataset.from_iterable([
             DatasetItem(id='000001', subset='train',
                 image=np.ones((5, 5, 3)),
-                annotations=[Label(12),
+                annotations=[
+                    Label(12),
                     Bbox(95, 71, 226, 313, label=12),
-                    Points([165, 184, 244, 176, 196, 249, 194, 271, 266, 260], label=12)],
+                    Points([165, 184, 244, 176, 196, 249, 194, 271, 266, 260],
+                        label=12)
+                ],
                 attributes={'5_o_Clock_Shadow': False, 'Arched_Eyebrows': True,
                     'Attractive': True, 'Bags_Under_Eyes': False, 'Bald': False,
                     'Bangs': False, 'Big_Lips': False, 'Big_Nose': False}
             ),
             DatasetItem(id='000002', subset='train',
                 image=np.ones((5, 5, 3)),
-                annotations=[Label(5),
+                annotations=[
+                    Label(5),
                     Bbox(72, 94, 221, 306, label=5),
-                    Points([140, 204, 220, 204, 168, 254, 146, 289, 226, 289], label=5)]
+                    Points([140, 204, 220, 204, 168, 254, 146, 289, 226, 289],
+                        label=5)
+                ]
             ),
             DatasetItem(id='000003', subset='val',
                 image=np.ones((5, 5, 3)),
-                annotations=[Label(2),
+                annotations=[
+                    Label(2),
                     Bbox(216, 59, 91, 126, label=2),
-                    Points([244, 104, 264, 105, 263, 121, 235, 134, 251, 140], label=2)],
+                    Points([244, 104, 264, 105, 263, 121, 235, 134, 251, 140],
+                        label=2)
+                ],
                 attributes={'5_o_Clock_Shadow': False, 'Arched_Eyebrows': False,
                     'Attractive': False, 'Bags_Under_Eyes': True, 'Bald': False,
                     'Bangs': False, 'Big_Lips': False, 'Big_Nose': True}
             ),
             DatasetItem(id='000004', subset='test',
                 image=np.ones((5, 5, 3)),
-                annotations=[Label(10),
+                annotations=[
+                    Label(10),
                     Bbox(622, 257, 564, 781, label=10),
-                    Points([796, 539, 984, 539, 930, 687, 762, 756, 915, 756], label=10)]
+                    Points([796, 539, 984, 539, 930, 687, 762, 756, 915, 756],
+                        label=10)
+                    ]
             ),
             DatasetItem(id='000005', subset='test',
                 image=np.ones((5, 5, 3)),
-                annotations=[Label(7),
+                annotations=[
+                    Label(7),
                     Bbox(236, 109, 120, 166, label=7),
-                    Points([273, 169, 328, 161, 298, 172, 283, 208, 323, 207], label=7)]
+                    Points([273, 169, 328, 161, 298, 172, 283, 208, 323, 207],
+                        label=7)
+                ]
             )
         ], categories=['class-%d' % i for i in range(13)])
 
@@ -66,34 +81,49 @@ class CelebaImporterTest(TestCase):
         expected_dataset = Dataset.from_iterable([
             DatasetItem(id='000001', subset='train',
                 image=np.ones((3, 4, 3)),
-                annotations=[Label(12),
-                    Points([69, 109, 106, 113, 77, 142, 73, 152, 108, 154], label=12)],
+                annotations=[
+                    Label(12),
+                    Points([69, 109, 106, 113, 77, 142, 73, 152, 108, 154],
+                        label=12)
+                ],
                 attributes={'5_o_Clock_Shadow': False, 'Arched_Eyebrows': True,
                     'Attractive': True, 'Bags_Under_Eyes': False, 'Bald': False,
                     'Bangs': False, 'Big_Lips': False, 'Big_Nose': False}
             ),
             DatasetItem(id='000002', subset='train',
                 image=np.ones((3, 4, 3)),
-                annotations=[Label(5),
-                    Points([69, 110, 107, 112, 81, 135, 70, 151, 108, 153], label=5)]
+                annotations=[
+                    Label(5),
+                    Points([69, 110, 107, 112, 81, 135, 70, 151, 108, 153],
+                        label=5)
+                ]
             ),
             DatasetItem(id='000003', subset='val',
                 image=np.ones((3, 4, 3)),
-                annotations=[Label(2),
-                    Points([76, 112, 104, 106, 108, 128, 74, 156, 98, 158], label=2)],
+                annotations=[
+                    Label(2),
+                    Points([76, 112, 104, 106, 108, 128, 74, 156, 98, 158],
+                        label=2)
+                ],
                 attributes={'5_o_Clock_Shadow': False, 'Arched_Eyebrows': False,
                     'Attractive': False, 'Bags_Under_Eyes': True, 'Bald': False,
                     'Bangs': False, 'Big_Lips': False, 'Big_Nose': True}
             ),
             DatasetItem(id='000004', subset='test',
                 image=np.ones((3, 4, 3)),
-                annotations=[Label(10),
-                    Points([72, 113, 108, 108, 101, 138, 71, 155, 101, 151], label=10)]
+                annotations=[
+                    Label(10),
+                    Points([72, 113, 108, 108, 101, 138, 71, 155, 101, 151],
+                        label=10)
+                ]
             ),
             DatasetItem(id='000005', subset='test',
                 image=np.ones((3, 4, 3)),
-                annotations=[Label(7),
-                    Points([66, 114, 112, 112, 86, 119, 71, 147, 104, 150], label=7)]
+                annotations=[
+                    Label(7),
+                    Points([66, 114, 112, 112, 86, 119, 71, 147, 104, 150],
+                        label=7)
+                    ]
             )
         ], categories=['class-%d' % i for i in range(13)])
 
