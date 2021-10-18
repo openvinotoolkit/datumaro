ba059ed08be5b032e7c14810c8c3226dff108e31
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2021-10-18 18:00:53 +0300

    Implement frame extractor

--- datumaro/plugins/video_formats.py
+++ datumaro/plugins/video_formats.py
@@ -2,23 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 
-import io
 import os.path as osp
 
 import av
 import cv2
 
 from datumaro.components.extractor import DatasetItem, Importer, SourceExtractor
-from datumaro.util.os_util import find_files
-
-# Taken from https://en.wikipedia.org/wiki/Comparison_of_video_container_formats
-# An extension does not define file contents, but it can be a good file filter
-VIDEO_EXTENSIONS = [
-    '3gp', '3g2', 'asf', 'wmv', 'avi', 'divx',
-    'evo', 'f4v', 'flv', 'mkv', 'mk3d', 'mp4', 'mpg', 'mpeg',
-    'm2p', 'ps', 'ts', 'm2ts', 'mxf', 'ogg', 'ogv', 'ogx',
-    'mov', 'qt', 'rmvb', 'vob', 'webm'
-]
 
 
 class RandomAccessIterator:
@@ -48,25 +37,6 @@ class RandomAccessIterator:
         self.iterator = iter(self.iterable)
         self.pos = -1
 
-class VideoDirImporter(Importer):
-    @classmethod
-    def find_sources(cls, path):
-        if not osp.isdir(path):
-            return []
-        return [{ 'url': path, 'format': 'video_dir' }]
-
-class VideoDirExtractor(SourceExtractor):
-    def __init__(self, url, subset=None, max_depth=None, exts=None):
-        super().__init__(subset=subset)
-
-        assert osp.isdir(url), url
-
-        for path in find_files(url, exts=exts or VIDEO_EXTENSIONS,
-                recursive=True, max_depth=max_depth):
-            item_id = osp.relpath(osp.splitext(path)[0], url)
-            self._items.append(DatasetItem(id=item_id, subset=self._subset,
-                video=path))
-
 
 class VideoFramesImporter(Importer):
     @classmethod
@@ -77,14 +47,26 @@ class VideoFramesImporter(Importer):
 
 class VideoFramesExtractor(SourceExtractor):
     def __init__(self, url, subset=None, name_pattern='%06d',
-            freq=None, start_frame=0, end_frame=None):
+            step=1, start_frame=0, end_frame=None):
         super().__init__(subset=subset)
 
         assert osp.isfile(url), url
 
         self._name_pattern = name_pattern
-        self._reader = VideoReader(url, freq=freq,
+        self._reader = VideoReader(url, step=step,
             start_frame=start_frame, end_frame=end_frame)
+
+    def __iter__(self):
+        frame_iter = iter(self._reader)
+
+        for frame_idx, _ in enumerate(frame_iter):
+            yield DatasetItem(id=self._name_pattern % frame_idx,
+                subset=self._subset,
+                image=self._lazy_get_frame(frame_iter, frame_idx))
+
+    @staticmethod
+    def _lazy_get_frame(frame_iter, idx):
+        return lambda _: frame_iter[idx]
 
 
 def rotate_image(image, angle):
@@ -101,27 +83,15 @@ def rotate_image(image, angle):
     return matrix
 
 class VideoReader:
-    def __init__(self, path, freq=None, start_frame=0, end_frame=None):
+    def __init__(self, path, step=1, start_frame=0, end_frame=None):
         self._path = path
-        self._freq = int(freq) if freq else None
+        self._step = int(step) if step else None
         self._start_frame = int(start_frame)
         self._end_frame = int(end_frame) if end_frame else None
-        self._container = None
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
-
-    def close(self):
-        if self._container is not None:
-            self._container.close()
-            self._container = None
 
     def _includes_frame(self, i):
         if self._start_frame <= i:
-            if (i - self._start_frame) % self._freq == 0:
+            if (i - self._start_frame) % self._step == 0:
                 if self._end_frame is None or i < self._end_frame:
                     return True
 
@@ -154,24 +124,14 @@ class VideoReader:
     def __iter__(self):
         yield from self._decode(self._get_av_container())
 
-    def get_progress(self, pos):
-        duration = self._get_duration()
-        return pos / duration if duration else None
-
-    def _open_av_container(self):
-        return av.open(self._path)
-
     def _get_av_container(self):
-        if self._container is None:
-            container = self._open_av_container()
+        container = av.open(self._path)
 
-            # Allow independent multithreaded frame decoding
-            container.streams.video[0].thread_type = 'AUTO'
+        # Allow independent multithreaded frame decoding
+        container.streams.video[0].thread_type = 'AUTO'
+        return container
 
-            self._container = container
-        return self._container
-
-    def _get_duration(self):
+    def get_duration(self):
         container = self._get_av_container()
         stream = container.streams.video[0]
 
@@ -183,26 +143,12 @@ class VideoReader:
             duration_str = stream.metadata.get("DURATION", None)
             tb_denominator = stream.time_base.denominator
             if duration_str and tb_denominator:
-                _hour, _min, _sec = duration_str.split(':')
-                duration_sec = 60*60*float(_hour) + 60*float(_min) + float(_sec)
+                h, m, s = duration_str.split(':')
+                duration_sec = 60 * 60 * float(h) + 60 * float(m) + float(s)
                 duration = duration_sec * tb_denominator
 
         return duration
 
-    def get_preview(self):
-        return next(iter(self))
-
     def get_image_size(self):
         image = next(iter(self))
-        return image.width, image.height
-
-class ByteVideoReader(VideoReader):
-    def __init__(self, file, path=None, freq=None, start_frame=0, end_frame=None):
-        super().__init__(path=path, freq=freq,
-            start_frame=start_frame, end_frame=end_frame)
-        self._file = file
-
-    def _open_av_container(self):
-        if isinstance(self._file, io.BytesIO):
-            self._file.seek(0) # required for re-reading
-        return av.open(self._file)
+        return (image.width, image.height)
