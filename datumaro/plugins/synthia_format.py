ba195cdd2f173168b09585470c08d1fe1d19baeb
yasakova-anastasia <anastasia.yasakova@intel.com>
2021-11-08 13:38:53 +0300

    add segmentation task

--- datumaro/plugins/synthia_format.py
+++ datumaro/plugins/synthia_format.py
@@ -2,97 +2,44 @@
 #
 # SPDX-License-Identifier: MIT
 
-from genericpath import isfile
+from collections import OrderedDict
 import os.path as osp
+
 import numpy as np
 
-from attr import attributes
-from collections import OrderedDict
-
 from datumaro.components.annotation import (
-    AnnotationType, Bbox, Cuboid3d, Label, LabelCategories, MaskCategories, Points, PointsCategories, Mask
+    AnnotationType, LabelCategories, Mask, MaskCategories,
 )
-from datumaro.components.errors import DatasetImportError
 from datumaro.components.extractor import DatasetItem, Importer, SourceExtractor
 from datumaro.plugins.camvid_format import parse_label_map
-from datumaro.util.image import find_images, load_image
 from datumaro.util import find
+from datumaro.util.image import find_images, load_image
 from datumaro.util.mask_tools import generate_colormap
-
-from glob import glob
 
 
 class SynthiaPath:
-    IMAGES_DIR = 'Img/img_celeba'
-    LABELS_FILE = 'Anno/identity_CelebA.txt'
-    BBOXES_FILE = 'Anno/list_bbox_celeba.txt'
-    ATTRS_FILE = 'Anno/list_attr_celeba.txt'
-    LANDMARKS_FILE = 'Anno/list_landmarks_celeba.txt'
-    SUBSETS_FILE = 'Eval/list_eval_partition.txt'
-    SUBSETS = {'0': 'train', '1': 'val', '2': 'test'}
-    BBOXES_HEADER = 'image_id x_1 y_1 width height'
-
-# SynthiaLabelMap = {
-#     0: 'Void',
-#     1: 'Sky',
-#     2: 'Building',
-#     3: 'Road',
-#     4: 'Sidewalk',
-#     5: 'Fence',
-#     6: 'Vegetation',
-#     7: 'Pole',
-#     8: 'Car',
-#     9: 'TrafficSign',
-#     10: 'Pedestrian',
-#     11: 'Bicycle',
-#     12: 'Lanemarking',
-#     15: 'TrafficLight',
-# }
-
-# SynthiaColorMap = {
-#     0: (0, 0, 0),
-#     1: (128, 128, 128),
-#     2: (128, 0, 0),
-#     3: (128, 64, 128),
-#     4: (0, 0, 192),
-#     5: (64, 64, 128),
-#     6: (128, 128, 0),
-#     7: (192, 192, 128),
-#     8: (64, 0, 128),
-#     9: (192, 128, 128),
-#     10: (64, 64, 0),
-#     11: (0, 128, 192),
-#     12: (0, 172, 0),
-#     15: (0, 128, 128),
-# }
+    IMAGES_DIR = 'RGB'
+    LABELS_SEGM_DIR = 'GT/LABELS'
 
 SynthiaLabelMap = OrderedDict([
     ('Void', (0, 0, 0)),
+    ('Sky', (128, 128, 128)),
+    ('Building', (128, 0, 0)),
     ('Road', (128, 64, 128)),
-    ('Sidewalk', (244, 35, 232)),
-    ('Building', (70, 70, 70)),
-    ('Wall', (102, 102, 156)),
-    ('Fence', (190, 153, 153)),
-    ('Pole', (153, 153, 153)),
-    ('TrafficLight', (250, 170, 30)),
-    ('TrafficSign', (220, 220, 0)),
-    ('Vegetation', (107, 142, 35)),
-    ('Terrain', (152, 251, 152)),
-    ('Sky', (70, 130, 180)),
-    ('Pedestrian', (220, 20, 60)),
-    ('Rider', (255, 0, 0)),
-    ('Car', (0, 0, 142)),
+    ('Sidewalk', (0, 0, 192)),
+    ('Fence', (64, 64, 128)),
+    ('Vegetation', (128, 128, 0)),
+    ('Pole', (192, 192, 128)),
+    ('Car', (64, 0, 128)),
     ('Truck', (0, 0, 70)),
-    ('Bus', (0, 60, 100)),
-    ('Train', (0, 80, 100)),
-    ('Motorcycle', (0, 0, 230)),
-    ('Bicycle', (119, 11, 32)),
-    ('Lanemarking', (0, 0, 142)),
-    ('licenseplate', (0, 0, 142)),
+    ('TrafficSign', (192, 128, 128)),
+    ('Pedestrian', (64, 64, 0)),
+    ('Bicycle', (0, 128, 192)),
+    ('Lanemarking', (0, 172, 0)),
+    ('TrafficLight', (0, 128, 128)),
 ])
 
-
-def make_cityscapes_categories(label_map=None):
+def make_categories(label_map=None):
     if label_map is None:
         label_map = SynthiaLabelMap
 
@@ -159,7 +106,7 @@ class SynthiaExtractor(SourceExtractor):
         if not osp.isdir(path):
             raise FileNotFoundError("Can't read dataset directory '%s'" % path)
 
-        super().__init__(subset=osp.basename(path))
+        super().__init__()
 
         self._categories = self._load_categories(path)
         self._items = list(self._load_items(path).values())
@@ -172,60 +119,35 @@ class SynthiaExtractor(SourceExtractor):
         else:
             label_map = SynthiaLabelMap
         self._labels = [label for label in label_map]
-        return make_cityscapes_categories(label_map)
+        return make_categories(label_map)
 
     def _load_items(self, root_dir):
+
+        image_dir = osp.join(root_dir, 'RGB')
+        if osp.isdir(image_dir):
+            images = {
+                osp.splitext(osp.relpath(p, image_dir))[0].replace('\\', '/'): p
+                for p in find_images(image_dir, recursive=True)
+            }
+        else:
+            images = {}
+
         items = {}
-        label_categories = self._categories[AnnotationType.label]
-        for dir_ in glob(osp.join(root_dir, '**', 'Information'), recursive=True):
-            image_dir = osp.join(osp.dirname(dir_), 'RGB')
-            if osp.isdir(image_dir):
-                images = {
-                    osp.join(osp.basename(osp.dirname(osp.dirname(p))),
-                    osp.splitext(osp.relpath(p, image_dir))[0].replace('\\', '/')): p
-                    for p in find_images(image_dir, recursive=True)
-                }
-            else:
-                images = {}
 
-            for labels_kitti_file in glob(osp.join(osp.dirname(dir_), 'labels_kitti', '**')):
-                annotations = []
-                group = 1
-                with open(labels_kitti_file, encoding='utf-8') as f:
-                    for line in f:
-                        line = line.split()
-                        label = label_categories.find(line[0])[0]
-                        if (label == None):
-                            label = label_categories.add(line[0])
-
-                        annotations.append(Bbox(float(line[4]), float(line[5]), float(line[6]), float(line[7]),
-                            label=label, group=group))
-                        annotations.append(Cuboid3d([float(line[8]), float(line[9]), float(line[10])],
-                            scale=[float(line[11]), float(line[12]), float(line[13])],
-                            rotation=[float(line[14]), float(line[14]), float(line[14])], group=group))
-
-                        group += 1
-
-                        item_id = osp.join(osp.basename(osp.dirname(osp.dirname(labels_kitti_file))),
-                            osp.splitext(osp.basename(labels_kitti_file))[0])
-
-                        items[item_id] = DatasetItem(id=item_id, subset=self._subset, image=images.get(item_id),
-                            annotations=annotations)
-
-            for gt_image in find_images(osp.join(osp.dirname(dir_), 'SemSeg')):
-                item_id = osp.join(osp.basename(osp.dirname(osp.dirname(gt_image))),
-                    osp.splitext(osp.basename(gt_image))[0])
-                annotations = items[item_id].annotations
-                instances_mask = load_image(gt_image, dtype=np.uint8)
-                segm_ids = np.unique(instances_mask[:,:,2])
-                # print(np.unique(instances_mask[:,:,0]))
-                # print(np.unique(instances_mask[:,:,1]))
-                # print(np.unique(instances_mask[:,:,2]))
+        gt_dir = osp.join(root_dir, SynthiaPath.LABELS_SEGM_DIR)
+        if osp.isdir(gt_dir):
+            gt_images = find_images(gt_dir, recursive=True)
+            for gt_img in gt_images:
+                item_id = osp.splitext(osp.relpath(gt_img, gt_dir))[0].replace('\\', '/')
+                anno = []
+                instances_mask = load_image(gt_img, dtype=np.uint16)[:,:,2]
+                segm_ids = np.unique(instances_mask)
                 for segm_id in segm_ids:
-                    annotations.append(Mask(
+                    anno.append(Mask(
                         image=self._lazy_extract_mask(instances_mask, segm_id),
                         label=segm_id))
-
+                items[item_id] = DatasetItem(id=item_id, image=images[item_id],
+                    annotations=anno)
         return items
 
     @staticmethod
@@ -235,4 +157,4 @@ class SynthiaExtractor(SourceExtractor):
 class SynthiaImporter(Importer):
     @classmethod
     def find_sources(cls, path):
-        return cls._find_sources_recursive(path, '', 'synthia')
+        return [{'url': path, 'format': 'synthia'}]
--- tests/test_synthia_format.py
+++ tests/test_synthia_format.py
@@ -4,17 +4,14 @@ import os.path as osp
 
 import numpy as np
 
-from datumaro.components.annotation import (
-    AnnotationType, Bbox, Cuboid3d, Label, LabelCategories, MaskCategories, Points, PointsCategories,
-)
+from datumaro.components.annotation import Mask
 from datumaro.components.dataset import Dataset
 from datumaro.components.extractor import DatasetItem
-import datumaro.plugins.synthia_format as Synthia
 from datumaro.plugins.synthia_format import SynthiaImporter
 from datumaro.util.test_utils import compare_datasets
+import datumaro.plugins.synthia_format as Synthia
 
 from .requirements import Requirements, mark_requirement
-from datumaro.util.mask_tools import generate_colormap
 
 DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets',
     'synthia_dataset')
@@ -23,42 +20,39 @@ class SynthiaImporterTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_import(self):
         expected_dataset = Dataset.from_iterable([
-            DatasetItem(id='19-10-2018_12-47-37/000101', subset='train',
-                image=np.ones((5, 5, 3)),
+            DatasetItem(id='Stereo_Left/Omni_B/000000',
+                image=np.ones((1, 5, 3)),
                 annotations=[
-                    Bbox(59.13, 228.35, 71.78, 265.96, label=10, group=1),
-                    Cuboid3d(position=[1.73, 0.54, 0.39],
-                        scale=[-11.79, 1.19, 41.49],
-                        rotation=[0.11, 0.11, 0.11], group=1),
-                    Bbox(311.52, 155.15, 317.13, 167.96, label=13, group=2),
-                    Cuboid3d(position=[0.64, 0.23, 0.25],
-                        scale=[-0.29, -3.72, 46.09],
-                        rotation=[-2.94, -2.94, -2.94], group=2)
+                    Mask(np.array([[1, 1, 0, 0, 0]]), label=1),
+                    Mask(np.array([[0, 0, 1, 1, 1]]), label=10),
                 ],
             ),
-            DatasetItem(id='19-10-2018_12-47-37/000102', subset='train',
-                image=np.ones((5, 5, 3)),
+            DatasetItem(id='Stereo_Left/Omni_B/000001',
+                image=np.ones((1, 5, 3)),
                 annotations=[
-                    Bbox(311.46, 154.88, 317.08, 167.72, label=13, group=1),
-                    Cuboid3d(position=[0.64, 0.23, 0.25],
-                        scale=[-0.29, -3.72, 45.95],
-                        rotation=[-2.94, -2.94, -2.94], group=1),
-                    Bbox(0.0, 234.16, 49.46, 263.56, label=8, group=2),
-                    Cuboid3d(position=[1.73, 2.09, 4.5],
-                        scale=[-18.95, 1.4, 54.22],
-                        rotation=[-3.14, -3.14, -3.14], group=2)
+                    Mask(np.array([[1, 0, 0, 0, 0]]), label=8),
+                    Mask(np.array([[0, 1, 1, 0, 0]]), label=11),
+                    Mask(np.array([[0, 0, 0, 1, 1]]), label=3),
                 ],
             ),
-            DatasetItem(id='19-10-2018_12-47-37/000103', subset='train',
-                image=np.ones((5, 5, 3)),
+            DatasetItem(id='Stereo_Left/Omni_F/000000',
+                image=np.ones((1, 5, 3)),
                 annotations=[
-                    Bbox(0.0, 234.13, 45.54, 263.64, label=8, group=1),
-                    Cuboid3d(position=[1.73, 2.09, 4.5],
-                        scale=[-19.13, 1.4, 54.03],
-                        rotation=[-3.14, -3.14, -3.14], group=1)
+                    Mask(np.array([[1, 1, 0, 0, 0]]), label=1),
+                    Mask(np.array([[0, 0, 1, 1, 0]]), label=2),
+                    Mask(np.array([[0, 0, 0, 0, 1]]), label=3),
+                ],
+            ),
+            DatasetItem(id='Stereo_Left/Omni_F/000001',
+                image=np.ones((1, 5, 3)),
+                annotations=[
+                    Mask(np.array([[1, 0, 0, 0, 0]]), label=1),
+                    Mask(np.array([[0, 1, 0, 0, 0]]), label=2),
+                    Mask(np.array([[0, 0, 1, 1, 0]]), label=15),
+                    Mask(np.array([[0, 0, 0, 0, 1]]), label=3),
                 ],
             )
-        ], categories=Synthia.make_cityscapes_categories())
+        ], categories=Synthia.make_categories())
 
         dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'synthia')
 
