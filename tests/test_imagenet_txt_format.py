9956f173f4cc24a37eaaf0e7b2340314f0e8a7af
Roman Donchenko <roman.donchenko@intel.com>
2021-11-19 19:42:28 +0300

    Add detection support for the camvid and imagenet_txt formats (#558)
    * format_detection: add a way to exclude some files in require_file
    
    * Add detection support for imagenet_txt and camvid
    
    * Update changelog entry

--- tests/test_imagenet_txt_format.py
+++ tests/test_imagenet_txt_format.py
@@ -200,4 +200,4 @@ class ImagenetTxtImporterTest(TestCase):
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_can_detect_imagenet(self):
         detected_formats = Environment().detect_dataset(DUMMY_DATASET_DIR)
-        self.assertIn(ImagenetTxtImporter.NAME, detected_formats)
+        self.assertEqual([ImagenetTxtImporter.NAME], detected_formats)
