8045c669a97ee921a06c36fa80201c52ca995b8e
yasakova-anastasia <anastasia.yasakova@intel.com>
2021-11-24 11:57:31 +0300

    add import for VoTT CSV

---
+++ datumaro/plugins/vott_csv_format.py
@@ -0,0 +1,74 @@
+# Copyright (C) 2021 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+import csv
+import os.path as osp
+
+from datumaro.components.annotation import AnnotationType, Bbox, LabelCategories
+from datumaro.components.extractor import DatasetItem, Importer, SourceExtractor
+from datumaro.components.format_detection import FormatDetectionContext
+from datumaro.util.image import find_images
+
+
+class VottCsvPath:
+    IMAGES_DIR = 'images'
+
+class VottCsvExtractor(SourceExtractor):
+    def __init__(self, path):
+        if not osp.isfile(path):
+            raise FileNotFoundError("Can't read annotation file '%s'" % path)
+
+        super().__init__(subset=osp.splitext(osp.basename(path))[0])
+
+        self._categories = { AnnotationType.label: LabelCategories() }
+        self._items = list(self._load_items(path).values())
+
+    def _load_items(self, path):
+        items = {}
+
+        image_dir = osp.join(osp.dirname(path), VottCsvPath.IMAGES_DIR, self._subset)
+
+        if osp.isdir(image_dir):
+            images = {
+                osp.splitext(osp.relpath(p, image_dir))[0].replace('\\', '/'): p
+                for p in find_images(image_dir, recursive=True)
+            }
+        else:
+            images = {}
+
+        label_categories = self._categories[AnnotationType.label]
+
+        with open(path, encoding='utf-8') as content:
+            anno_table = list(csv.DictReader(content))
+        for row in anno_table:
+            item_id = osp.splitext(row['image'])[0]
+
+            if item_id not in items:
+                items[item_id] = DatasetItem(id=item_id, subset=self._subset,
+                    image=images.get(item_id))
+
+            annotations = items[item_id].annotations
+            if (len(row) == 6):
+                label_name = row['label']
+                label_idx = label_categories.find(label_name)[0]
+                if label_idx is None:
+                    label_idx = label_categories.add(label_name)
+
+                x = float(row['xmin'])
+                y = float(row['ymin'])
+                w = float(row['xmax']) - x
+                h = float(row['ymax']) - y
+
+                annotations.append(Bbox(x, y, w, h, label=label_idx))
+
+        return items
+
+class VottCsvImporter(Importer):
+    @classmethod
+    def find_sources(cls, path):
+        return cls._find_sources_recursive(path, '.csv', 'vott_csv')
+
+    @classmethod
+    def detect(cls, context: FormatDetectionContext) -> None:
+        context.require_file(f'*.csv')
---
+++ tests/test_vott_csv_format.py
@@ -0,0 +1,56 @@
+from unittest import TestCase
+import os.path as osp
+
+import numpy as np
+
+from datumaro.components.annotation import Bbox
+from datumaro.components.dataset import Dataset
+from datumaro.components.environment import Environment
+from datumaro.components.extractor import DatasetItem
+from datumaro.plugins.vott_csv_format import VottCsvImporter
+from datumaro.util.test_utils import compare_datasets
+
+from .requirements import Requirements, mark_requirement
+
+DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets',
+    'vott_csv_dataset')
+
+class VottCsvImporterTest(TestCase):
+    @mark_requirement(Requirements.DATUM_475)
+    def test_can_import(self):
+        expected_dataset = Dataset.from_iterable([
+            DatasetItem(id='img0001', subset='test',
+                image=np.ones((5, 5, 3)),
+                annotations=[
+                    Bbox(10, 5, 10, 2, label=0)
+                ]
+            ),
+            DatasetItem(id='img0002', subset='test',
+                image=np.ones((5, 5, 3)),
+                annotations=[
+                    Bbox(11.5, 12, 10.2, 20.5, label=1),
+                ]
+            ),
+            DatasetItem(id='img0003', subset='train',
+                image=np.ones((5, 5, 3)),
+                annotations=[
+                    Bbox(6.7, 10.3, 3.3, 4.7, label=0),
+                    Bbox(13.7, 20.2, 31.9, 43.4, label=1),
+                ]
+            ),
+            DatasetItem(id='img0004', subset='train',
+                image=np.ones((5, 5, 3)),
+                annotations=[
+                    Bbox(1, 2, 1, 2, label=0),
+                ]
+            )
+        ], categories=['helmet', 'person'])
+
+        dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'vott_csv')
+
+        compare_datasets(self, expected_dataset, dataset, require_images=True)
+
+    @mark_requirement(Requirements.DATUM_475)
+    def test_can_detect(self):
+        detected_formats = Environment().detect_dataset(DUMMY_DATASET_DIR)
+        self.assertIn(VottCsvImporter.NAME, detected_formats)
