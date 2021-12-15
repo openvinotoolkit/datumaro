7450b8f696704b7cfd403b373454b7f053980f9d
Roman Donchenko <roman.donchenko@intel.com>
2021-12-15 15:41:19 +0300

    Add tests for the dataset download functionality

---
+++ tests/cli/test_download.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+from unittest.case import skipIf
+import os.path as osp
+
+from datumaro.components.dataset import Dataset
+from datumaro.components.extractor_tfds import (
+    TFDS_EXTRACTOR_AVAILABLE, make_tfds_extractor,
+)
+from datumaro.util.test_utils import TestDir, compare_datasets, mock_tfds_data
+from datumaro.util.test_utils import run_datum as run
+
+from ..requirements import Requirements, mark_requirement
+
+
+@skipIf(not TFDS_EXTRACTOR_AVAILABLE, reason="TFDS is not installed")
+class DownloadTest(TestCase):
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_download(self):
+        with TestDir() as test_dir, mock_tfds_data():
+            expected_dataset = Dataset(make_tfds_extractor('mnist'))
+
+            run(self, 'download',
+                '-i', 'tfds:mnist', '-f', 'datumaro', '-o', test_dir,
+                '--', '--save-images')
+
+            actual_dataset = Dataset.load(test_dir)
+            compare_datasets(self, expected_dataset, actual_dataset,
+                require_images=True)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_download_fails_on_existing_dir_without_overwrite(self):
+        with TestDir() as test_dir:
+            with open(osp.join(test_dir, 'text.txt'), 'w'):
+                pass
+
+            run(self, 'download',
+                '-i', 'tfds:mnist', '-f', 'datumaro', '-o', test_dir,
+                expected_code=1)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_download_works_on_existing_dir_without_overwrite(self):
+        with TestDir() as test_dir, mock_tfds_data():
+            with open(osp.join(test_dir, 'text.txt'), 'w'):
+                pass
+
+            run(self, 'download',
+                '-i', 'tfds:mnist', '-f', 'datumaro', '-o', test_dir,
+                '--overwrite', '--', '--save-images')
