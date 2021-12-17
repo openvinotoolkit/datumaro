6856538d3ac84748a7d40660ad5257c6e68c4bba
yasakova-anastasia <anastasia.yasakova@intel.com>
2021-12-17 15:23:46 +0300

    add numpy annotation files

--- datumaro/plugins/mpii_format.py
+++ datumaro/plugins/mpii_format.py
@@ -7,46 +7,101 @@ import os.path as osp
 
 import numpy as np
 
-from datumaro.components.annotation import Points
-from datumaro.components.extractor import DatasetItem, Importer, SourceExtractor
+from datumaro.components.annotation import Bbox, Points, PointsCategories
+from datumaro.components.extractor import (
+    AnnotationType, DatasetItem, Importer, SourceExtractor,
+)
 from datumaro.components.format_detection import FormatDetectionContext
 from datumaro.components.media import Image
 
 
 class MpiiPath:
-    ANNO_FILE_PREFIX = 'annot/mpii_'
+    ANNOTATION_FILE = 'mpii_annotations.json'
+    HEADBOXES_FILE = 'mpii_headboxes.npy'
+    VISIBILITY_FILE = 'jnt_visible.npy'
+    POS_GT_FILE = 'mpii_pos_gt.npy'
+
+MpiiPointsCategories = [(0, ['r_ankle']), (1, ['r_knee']), (2, ['r_hip']),
+    (3, ['l_hip']), (4, ['l_knee']), (5, ['l_ankle']), (6, ['pelvis']),
+    (7, ['thorax']), (8, ['upper_neck']), (9, ['head top']), (10, ['r_wrist']),
+    (11, ['r_elbow']), (12, ['r_shoulder']), (13, ['l_shoulder']),
+    (14, ['l_elbow']), (15, ['l_wrist'])]
 
 class MpiiExtractor(SourceExtractor):
     def __init__(self, path):
         if not osp.isfile(path):
             raise FileNotFoundError("Can't read annotation file '%s'" % path)
 
-        super().__init__(subset=osp.splitext(osp.basename(path))[0].
-            split('_', maxsplit=1)[1])
+        super().__init__()
+
+        self._categories = { AnnotationType.points:
+            PointsCategories.from_iterable(MpiiPointsCategories) }
 
         self._items = list(self._load_items(path).values())
 
     def _load_items(self, path):
         items = {}
 
+        root_dir = osp.dirname(path)
+
+        hb_path = osp.join(root_dir, MpiiPath.HEADBOXES_FILE)
+        if osp.isfile(hb_path):
+            headboxes = np.load(hb_path)
+        else:
+            headboxes = []
+
+        vis_path = osp.join(root_dir, MpiiPath.VISIBILITY_FILE)
+        if osp.isfile(vis_path):
+            c = np.load(vis_path)
+            visibility = np.load(vis_path).T
+        else:
+            visibility = []
+
+        pos_gt_path = osp.join(root_dir, MpiiPath.POS_GT_FILE)
+        if osp.isfile(pos_gt_path):
+            r = np.load(pos_gt_path)
+            gt_pose = np.transpose(np.load(pos_gt_path), (2, 0, 1))
+        else:
+            gt_pose = []
+
         with open(path) as f:
-            for ann in json.load(f):
+            for i, ann in enumerate(json.load(f)):
                 item_id = osp.splitext(ann.get('img_paths'))[0]
 
-                center = ann['objpos']
-                scale = float(ann['scale_provided'])
-                keypoints = np.array(ann['joint_self'])
-                keypoints = keypoints.reshape(keypoints.shape[0] * keypoints.shape[1])
-                points = [p for i, p in enumerate(keypoints) if i % 3 != 2]
-                vis = keypoints[2::3]
+                center = ann.get('objpos')
+                scale = float(ann.get('scale_provided'))
+
+                if np.size(gt_pose):
+                    points = gt_pose[i]
+                    points = points.reshape(points.shape[0] * points.shape[1])
+
+                    if np.size(visibility):
+                        vis = visibility[i]
+                    else:
+                        vis = np.ones(len(points) // 2)
+                else:
+                    keypoints = np.array(ann.get('joint_self'))
+                    keypoints = keypoints.reshape(keypoints.shape[0] * keypoints.shape[1])
+                    points = [p for i, p in enumerate(keypoints) if i % 3 != 2]
+
+                    vis = keypoints[2::3]
+                    if np.size(visibility):
+                        vis = visibility[i]
+
                 vis = [int(val) for val in vis]
 
-                root_dir = osp.dirname(osp.dirname(path))
+                annotations = [Points(points, vis, attributes={'center': center,
+                    'scale': scale})]
+
+                if np.size(headboxes):
+                    bbox = headboxes[:, :, i]
+                    annotations.append(Bbox(bbox[0][0], bbox[0][1],
+                        bbox[1][0] - bbox[0][0], bbox[1][1] - bbox[0][1]))
+
 
                 items[item_id] = DatasetItem(id=item_id, subset=self._subset,
                     image=Image(path=osp.join(root_dir, ann.get('img_paths'))),
-                    annotations=[Points(points, vis, attributes={'center': center,
-                        'scale': scale})])
+                    annotations=annotations)
 
         return items
 
@@ -57,4 +112,4 @@ class MpiiImporter(Importer):
 
     @classmethod
     def detect(cls, context: FormatDetectionContext) -> None:
-        context.require_file(MpiiPath.ANNO_FILE_PREFIX + '*' + '.json')
+        context.require_file(MpiiPath.ANNOTATION_FILE)
--- tests/test_mpii_format.py
+++ tests/test_mpii_format.py
@@ -3,23 +3,72 @@ import os.path as osp
 
 import numpy as np
 
-from datumaro.components.annotation import Points
+from datumaro.components.annotation import Bbox, Points, PointsCategories
 from datumaro.components.dataset import Dataset
 from datumaro.components.environment import Environment
-from datumaro.components.extractor import DatasetItem
-from datumaro.plugins.mpii_format import MpiiImporter
+from datumaro.components.extractor import AnnotationType, DatasetItem
+from datumaro.plugins.mpii_format import MpiiImporter, MpiiPointsCategories
 from datumaro.util.test_utils import compare_datasets
 
 from .requirements import Requirements, mark_requirement
 
-DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), 'assets', 'mpii_dataset')
+DUMMY_DATASET_DIR_WITH_NUMPY_FILES = osp.join(osp.dirname(__file__), 'assets',
+    'mpii_dataset', 'dataset_with_numpy_files')
+DUMMY_DATASET_DIR_WO_NUMPY_FILES = osp.join(osp.dirname(__file__), 'assets',
+    'mpii_dataset', 'dataset_wo_numpy_files')
 
 class MpiiImporterTest(TestCase):
     @mark_requirement(Requirements.DATUM_580)
-    def test_can_import(self):
+    def test_can_import_dataset_witn_numpy_files(self):
         expected_dataset = Dataset.from_iterable([
-            DatasetItem(id='015601864',
-                subset='train', image=np.ones((5, 5, 3)),
+            DatasetItem(id='000000001', image=np.ones((5, 5, 3)),
+                annotations=[
+                    Points([620.0, 394.0, 616.0, 269.0, 573.0, 185.0, 647.0,
+                        188.0, 661.0, 221.0, 656.0, 231.0, 610.0, 187.0,
+                        647.0, 176.0, 637.02, 189.818, 695.98, 108.182,
+                        606.0, 217.0, 553.0, 161.0, 601.0, 167.0, 692.0,
+                        185.0, 693.0, 240.0, 688.0, 313.0],
+                        [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        attributes={'center': [594.000,257.000], 'scale': 3.021}),
+                    Bbox(615, 218.65, 288.4, 286.95)
+                ]
+            ),
+            DatasetItem(id='000000002', image=np.ones((5, 5, 3)),
+                annotations=[
+                    Points([650.0, 424.0, 646.0, 309.0, 603.0, 215.0, 677.0,
+                        218.0, 691.0, 251.0, 686.0, 261.0, 640.0, 217.0,
+                        677.0, 216.0, 667.02, 219.818, 725.98, 138.182,
+                        636.0, 247.0, 583.0, 191.0, 631.0, 197.0, 722.0,
+                        215.0, 723.0, 270.0, 718.0, 343.0],
+                        [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        attributes={'center': [624.000,287.000], 'scale': 3.7}),
+                    Bbox(101.1, 33.3, 113.9, 81.4)
+                ]
+            ),
+            DatasetItem(id='000000003', image=np.ones((5, 5, 3)),
+                annotations=[
+                    Points([590.0, 364.0, 586.0, 239.0, 533.0, 155.0, 617.0,
+                        158.0, 631.0, 191.0, 626.0, 201.0, 580.0, 157.0,
+                        617.0, 146.0, 607.02, 159.818, 645.98, 68.182,
+                        576.0, 187.0, 532.0, 131.0, 571.0, 137.0, 662.0,
+                        155.0, 663.0, 210.0, 658.0, 283.0],
+                        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+                        attributes={'center': [564.000,227.000], 'scale': 3.2}),
+                    Bbox(313.3, 512.43, 220.7, 121.57)
+                ]
+            )
+        ], categories={
+            AnnotationType.points: PointsCategories.from_iterable(MpiiPointsCategories)
+        })
+
+        dataset = Dataset.import_from(DUMMY_DATASET_DIR_WITH_NUMPY_FILES, 'mpii')
+
+        compare_datasets(self, expected_dataset, dataset, require_images=True)
+
+    @mark_requirement(Requirements.DATUM_580)
+    def test_can_import_dataset_wo_numpy_files(self):
+        expected_dataset = Dataset.from_iterable([
+            DatasetItem(id='000000001', image=np.ones((5, 5, 3)),
                 annotations=[
                     Points([620.0, 394.0, 616.0, 269.0, 573.0, 185.0, 647.0,
                         188.0, 661.0, 221.0, 656.0, 231.0, 610.0, 187.0,
@@ -29,15 +78,45 @@ class MpiiImporterTest(TestCase):
                         [1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
                         attributes={'center': [594.000,257.000], 'scale': 3.021})
                 ]
+            ),
+            DatasetItem(id='000000002', image=np.ones((5, 5, 3)),
+                annotations=[
+                    Points([650.0, 424.0, 646.0, 309.0, 603.0, 215.0, 677.0,
+                        218.0, 691.0, 251.0, 686.0, 261.0, 640.0, 217.0,
+                        677.0, 216.0, 667.02, 219.818, 725.98, 138.182,
+                        636.0, 247.0, 583.0, 191.0, 631.0, 197.0, 722.0,
+                        215.0, 723.0, 270.0, 718.0, 343.0],
+                        [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                        attributes={'center': [624.000,287.000], 'scale': 3.7})
+                ]
+            ),
+            DatasetItem(id='000000003', image=np.ones((5, 5, 3)),
+                annotations=[
+                    Points([590.0, 364.0, 586.0, 239.0, 533.0, 155.0, 617.0,
+                        158.0, 631.0, 191.0, 626.0, 201.0, 580.0, 157.0,
+                        617.0, 146.0, 607.02, 159.818, 645.98, 68.182,
+                        576.0, 187.0, 532.0, 131.0, 571.0, 137.0, 662.0,
+                        155.0, 663.0, 210.0, 658.0, 283.0],
+                        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+                        attributes={'center': [564.000,227.000], 'scale': 3.2})
+                ]
             )
-        ])
+        ], categories={
+            AnnotationType.points: PointsCategories.from_iterable(MpiiPointsCategories)
+        })
 
-        dataset = Dataset.import_from(DUMMY_DATASET_DIR, 'mpii')
+        dataset = Dataset.import_from(DUMMY_DATASET_DIR_WO_NUMPY_FILES, 'mpii')
 
         compare_datasets(self, expected_dataset, dataset, require_images=True)
 
+    @mark_requirement(Requirements.DATUM_580)
+    def test_can_detect_dataset_with_numpy_files(self):
+        detected_formats = Environment().detect_dataset(
+            DUMMY_DATASET_DIR_WITH_NUMPY_FILES)
+        self.assertEqual([MpiiImporter.NAME], detected_formats)
 
     @mark_requirement(Requirements.DATUM_580)
-    def test_can_detect(self):
-        detected_formats = Environment().detect_dataset(DUMMY_DATASET_DIR)
+    def test_can_detect_dataset_wo_numpy_files(self):
+        detected_formats = Environment().detect_dataset(
+            DUMMY_DATASET_DIR_WO_NUMPY_FILES)
         self.assertEqual([MpiiImporter.NAME], detected_formats)
