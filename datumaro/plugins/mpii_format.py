9c6fc7c0789b8c4a3f979cf3d73dc8e91418b2ab
yasakova-anastasia <anastasia.yasakova@intel.com>
2021-12-22 10:30:15 +0300

    style fixes

--- datumaro/plugins/mpii_format.py
+++ datumaro/plugins/mpii_format.py
@@ -23,11 +23,31 @@ class MpiiPath:
     VISIBILITY_FILE = 'jnt_visible.npy'
     POS_GT_FILE = 'mpii_pos_gt.npy'
 
-MPII_POINTS_LABELS = ['r_ankle', 'r_knee', 'r_hip', 'l_hip', 'l_knee', 'l_ankle',
-            'pelvis', 'thorax', 'upper_neck', 'head top', 'r_wrist',
-            'r_elbow', 'r_shoulder', 'l_shoulder', 'l_elbow', 'l_wrist'],
-MPI_POINTS_JOINTS = [(0, 1), (1, 2), (2, 6), (3, 4), (3, 6), (4, 5), (6, 7), (7, 8),
-            (8, 9), (8, 12), (8, 13), (10, 11), (11, 12), (13, 14), (14, 15)]
+MPII_POINTS_LABELS = [
+    'r_ankle',
+    'r_knee',
+    'r_hip',
+    'l_hip',
+    'l_knee',
+    'l_ankle',
+    'pelvis',
+    'thorax',
+    'upper_neck',
+    'head top',
+    'r_wrist',
+    'r_elbow',
+    'r_shoulder',
+    'l_shoulder',
+    'l_elbow',
+    'l_wrist'
+]
+
+MPI_POINTS_JOINTS = [
+    (0, 1), (1, 2), (2, 6), (3, 4),
+    (3, 6), (4, 5), (6, 7), (7, 8),
+    (8, 9), (8, 12), (8, 13), (10, 11),
+    (11, 12), (13, 14), (14, 15)
+]
 
 
 class MpiiExtractor(SourceExtractor):
