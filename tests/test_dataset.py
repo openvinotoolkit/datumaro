10b12aa107d4ef8fde4baec4e1719eb88ea42e96
kirill.sizov <kirill.sizov@intel.com>
2022-01-18 12:19:37 +0300

    Add test

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -176,6 +176,21 @@ class DatasetTest(TestCase):
             self.assertEqual(DEFAULT_FORMAT, detected_format)
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_can_detect_with_nested_folder_and_multiply_matches(self):
+        dataset = Dataset.from_iterable([
+            DatasetItem(id=1, image=np.ones((3, 3, 3)),
+                annotations=[ Label(2) ]),
+        ], categories=['a', 'b', 'c'])
+
+        with TestDir() as test_dir:
+            dataset_path = osp.join(test_dir, 'a', 'b')
+            dataset.export(dataset_path, 'coco', save_images=True)
+
+            detected_format = Dataset.detect(test_dir)
+
+            self.assertEqual('coco', detected_format)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_cannot_detect_for_non_existent_path(self):
         with TestDir() as test_dir:
             dataset_path = osp.join(test_dir, 'a')
