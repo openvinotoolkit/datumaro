5bcca3ad1e9d8903037d6f6caece17286fcc3797
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2022-01-21 16:27:07 +0300

    fix imports

--- datumaro/__init__.py
+++ datumaro/__init__.py
@@ -7,14 +7,14 @@ import datumaro.components.operations as ops
 import datumaro.components.project as project
 
 from .components.annotation import *
+from .components.cli_plugin import CliPlugin
 from .components.converter import Converter
 from .components.dataset import (
     Dataset, DatasetPatch, DatasetSubset, IDataset, ItemStatus, eager_mode,
 )
 from .components.environment import Environment, PluginRegistry
 from .components.extractor import *
-from .components.hl_ops import * #pylint: disable=redefined-builtin
+from .components.hl_ops import *  # pylint: disable=redefined-builtin
 from .components.launcher import Launcher, ModelTransform
 from .components.media import *
 from .components.validator import Validator
-from .components.cli_plugin import CliPlugin
