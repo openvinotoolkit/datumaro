821289844f9cd1b84fac6ba96a15bf556154fa9b
yasakova-anastasia <anastasia.yasakova@intel.com>
2022-01-26 13:57:53 +0300

    Remove changes in VOC converter

--- datumaro/plugins/voc_format/converter.py
+++ datumaro/plugins/voc_format/converter.py
@@ -578,7 +578,9 @@ class VocConverter(Converter):
 
         # There must always be a label with color (0, 0, 0) at index 0
         bg_label = find(label_map.items(), lambda x: x[1][0] == (0, 0, 0))
-        if bg_label is None:
+        if bg_label is not None:
+            bg_label = bg_label[0]
+        else:
             bg_label = 'background'
             if bg_label not in label_map:
                 has_colors = any(v[0] is not None for v in label_map.values())
