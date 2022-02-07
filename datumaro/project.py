a29251e0ec38645a075a827f9fe3314b0ad48987
Maxim Zhiltsov <maxim.zhiltsov@intel.com>
2022-02-07 11:07:38 +0300

    Fix module aliases in new API

---
+++ datumaro/project.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# pylint: disable=unused-import
+
+# This module is a usability proxy for components.project
+
+from .components.project import (
+    BuildStageType, DiffStatus, IgnoreMode, ObjectId, Pipeline, Project,
+    ProjectBuilder, ProjectBuildTargets, ProjectSourceDataset, Revision, Tree,
+)
