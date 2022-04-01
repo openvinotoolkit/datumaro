9b477f04d88333e9837faf4f6dc23caf62cc6131
yasakova-anastasia <anastasia.yasakova@intel.com>
2022-04-01 10:14:37 +0300

    Add tests for Cityscapes

--- tests/test_cityscapes_format.py
+++ tests/test_cityscapes_format.py
@@ -23,8 +23,13 @@ from datumaro.util.test_utils import IGNORE_ALL, TestDir, check_save_and_load, c
 from .requirements import Requirements, mark_requirement
 
 DUMMY_DATASET_DIR = osp.join(osp.dirname(__file__), "assets", "cityscapes_dataset", "dataset")
+
 DUMMY_TRAIN_DATASET_DIR = osp.join(
     osp.dirname(__file__), "assets", "cityscapes_dataset", "train_dataset"
+)
+
+MANGLING_DATASET_DIR = osp.join(
+    osp.dirname(__file__), "assets", "cityscapes_dataset", "mangling_dataset"
 )
 
 
@@ -738,4 +743,31 @@ class CityscapesConverterTest(TestCase):
                 TestExtractor(),
                 partial(CityscapesConverter.convert, label_map="cityscapes"),
                 test_dir,
+            )
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_can_convert_to_cityscapes(self):
+        source_dataset = Dataset.import_from(MANGLING_DATASET_DIR, "cityscapes")
+
+        with TestDir() as test_dir:
+            source_dataset.export(test_dir, "cityscapes", save_media=True)
+            parsed_dataset = Dataset.import_from(test_dir, "cityscapes")
+
+            compare_datasets(self, source_dataset, parsed_dataset, require_media=True)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_can_convert_to_pascal_voc(self):
+        source_dataset = Dataset.import_from(MANGLING_DATASET_DIR, "cityscapes")
+
+        with TestDir() as test_dir:
+            source_dataset.export(test_dir, "coco_panoptic", save_media=True)
+            parsed_dataset = Dataset.import_from(test_dir, "coco_panoptic")
+
+            compare_datasets(
+                self,
+                source_dataset,
+                parsed_dataset,
+                require_media=True,
+                ignored_attrs={"id"},
+                externally_comparison=True,
             )
