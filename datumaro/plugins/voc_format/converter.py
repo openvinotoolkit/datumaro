227841d50266ac4ce29508ecad62875ca092686c
Emily <emily.chun@intel.com>
2022-09-05 20:17:02 +0900

    Fix CVS-85120 Cannot export detection->classification dataset

--- datumaro/plugins/voc_format/converter.py
+++ datumaro/plugins/voc_format/converter.py
@@ -234,7 +234,7 @@ class VocConverter(Converter):
 
                     self._export_annotations(item, image_filename=image_filename, lists=lists)
                 except Exception as e:
-                    self._report_item_error(e, item_id=(item.id, item.subset))
+                    self._ctx.error_policy.report_item_error(e, item_id=(item.id, item.subset))
 
             if self._tasks & {
                 VocTask.classification,
@@ -343,6 +343,8 @@ class VocConverter(Converter):
                     present = 0
                     if action in attr:
                         present = _convert_attr(action, attr, lambda v: int(v is True), 0)
+                        if action.isdigit():
+                            action = '_' + action
                         ET.SubElement(actions_elem, action).text = "%d" % present
 
                     objects_with_actions[new_obj_id][action] = present
@@ -461,7 +463,7 @@ class VocConverter(Converter):
             if not objs:
                 return
             for obj_id, obj_actions in objs.items():
-                presented = obj_actions[action]
+                presented = obj_actions.get(action)
                 f.write("%s %s % d\n" % (item, 1 + obj_id, 1 if presented else -1))
 
         all_actions = {
