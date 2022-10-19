787254c8157ce51d10306159da76d8fe9aec31c2
Kim, Vinnam <vinnam.kim@intel.com>
2022-10-19 18:21:23 +0900

    Add Label, Points, Polygon, PolyLine, and Caption unit tests
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -4,7 +4,7 @@ from unittest import TestCase, mock
 import numpy as np
 from matplotlib.figure import Figure
 
-from datumaro.components.annotation import Bbox
+from datumaro.components.annotation import Bbox, Caption, Label, Points, PolyLine, Polygon
 from datumaro.components.dataset import Dataset
 from datumaro.components.extractor import DatasetItem
 from datumaro.components.media import Image
@@ -20,7 +20,17 @@ class GridSizeTestCase:
 
 
 class VisualizerTestBase:
-    def test_vis_one_sample(self, mocked: mock.MagicMock):
+    DEFAULT_GRID_SIZE_TEST_CASES = [
+        GridSizeTestCase((None, None), (3, 2)),
+        GridSizeTestCase((3, None), (3, 2)),
+        GridSizeTestCase((None, 2), (3, 2)),
+        GridSizeTestCase((3, 2), (3, 2)),
+        GridSizeTestCase((None, 5), (1, 5)),
+        GridSizeTestCase((5, 1), (5, 1)),
+        GridSizeTestCase((5, 1), (5, 1)),
+    ]
+
+    def test_vis_one_sample(self, mocked: mock.MagicMock, check_z_order: bool = True):
         visualizer = Visualizer(self.dataset)
 
         for item in self.items:
@@ -30,8 +40,9 @@ class VisualizerTestBase:
             assert mocked.call_count == len(item.annotations)
 
             # Check z-order
-            called_z_order = [call[0][0].z_order for call in mocked.call_args_list]
-            assert sorted(called_z_order) == called_z_order
+            if check_z_order:
+                called_z_order = [call[0][0].z_order for call in mocked.call_args_list]
+                assert sorted(called_z_order) == called_z_order
 
             mocked.reset_mock()
 
@@ -71,6 +82,144 @@ class VisualizerTestBase:
             _check(test_case.infer_grid_size, test_case.expected_grid_size)
 
 
+class LabelVisualizerTest(TestCase, VisualizerTestBase):
+    @classmethod
+    def setUpClass(cls):
+        cls.subset = "train"
+        cls.items = [
+            DatasetItem(
+                "image_%03d" % img_idx,
+                subset=cls.subset,
+                media=Image(data=np.ones((4, 6, 3))),
+                annotations=[
+                    Label(
+                        label=label_idx,
+                        id=img_idx * img_idx + label_idx,
+                        group=1,
+                        attributes={},
+                    )
+                    for label_idx in range(img_idx)
+                ],
+            )
+            for img_idx in range(1, 6)
+        ]
+        cls.dataset = Dataset.from_iterable(cls.items)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    @mock.patch("datumaro.components.visualizer.Visualizer._draw_label")
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        super().test_vis_one_sample(mocked, check_z_order=False)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_vis_gallery(self):
+        super().test_vis_gallery(self.DEFAULT_GRID_SIZE_TEST_CASES)
+
+
+class PointsVisualizerTest(TestCase, VisualizerTestBase):
+    @classmethod
+    def setUpClass(cls):
+        cls.subset = "train"
+        cls.items = [
+            DatasetItem(
+                "image_%03d" % img_idx,
+                subset=cls.subset,
+                media=Image(data=np.ones((4, 6, 3))),
+                annotations=[
+                    Points(
+                        np.random.random_integers(0, 6, size=10 * 2),
+                        label=label_idx,
+                        id=img_idx * img_idx + label_idx,
+                        group=1,
+                        z_order=label_idx,
+                        attributes={},
+                    )
+                    for label_idx in range(img_idx)
+                ],
+            )
+            for img_idx in range(1, 6)
+        ]
+        cls.dataset = Dataset.from_iterable(cls.items)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    @mock.patch("datumaro.components.visualizer.Visualizer._draw_points")
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        super().test_vis_one_sample(mocked, check_z_order=True)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_vis_gallery(self):
+        super().test_vis_gallery(self.DEFAULT_GRID_SIZE_TEST_CASES)
+
+
+class PolygonVisualizerTest(TestCase, VisualizerTestBase):
+    @classmethod
+    def setUpClass(cls):
+        cls.subset = "train"
+        cls.items = [
+            DatasetItem(
+                "image_%03d" % img_idx,
+                subset=cls.subset,
+                media=Image(data=np.ones((4, 6, 3))),
+                annotations=[
+                    Polygon(
+                        np.random.random_integers(0, 6, size=10 * 2),
+                        label=label_idx,
+                        id=img_idx * img_idx + label_idx,
+                        group=1,
+                        z_order=label_idx,
+                        attributes={},
+                    )
+                    for label_idx in range(img_idx)
+                ],
+            )
+            for img_idx in range(1, 6)
+        ]
+        cls.dataset = Dataset.from_iterable(cls.items)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    @mock.patch("datumaro.components.visualizer.Visualizer._draw_polygon")
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        super().test_vis_one_sample(mocked, check_z_order=True)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_vis_gallery(self):
+        super().test_vis_gallery(self.DEFAULT_GRID_SIZE_TEST_CASES)
+
+
+class PolyLineVisualizerTest(TestCase, VisualizerTestBase):
+    @classmethod
+    def setUpClass(cls):
+        cls.subset = "train"
+        cls.items = [
+            DatasetItem(
+                "image_%03d" % img_idx,
+                subset=cls.subset,
+                media=Image(data=np.ones((4, 6, 3))),
+                annotations=[
+                    PolyLine(
+                        np.random.random_integers(0, 6, size=10 * 2),
+                        label=label_idx,
+                        id=img_idx * img_idx + label_idx,
+                        group=1,
+                        z_order=label_idx,
+                        attributes={},
+                    )
+                    for label_idx in range(img_idx)
+                ],
+            )
+            for img_idx in range(1, 6)
+        ]
+        cls.dataset = Dataset.from_iterable(cls.items)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    @mock.patch("datumaro.components.visualizer.Visualizer._draw_polygon")
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        super().test_vis_one_sample(mocked, check_z_order=True)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_vis_gallery(self):
+        super().test_vis_gallery(self.DEFAULT_GRID_SIZE_TEST_CASES)
+
+
 class BboxVisualizerTest(TestCase, VisualizerTestBase):
     @classmethod
     def setUpClass(cls):
@@ -106,14 +255,36 @@ class BboxVisualizerTest(TestCase, VisualizerTestBase):
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_vis_gallery(self):
-        super().test_vis_gallery(
-            [
-                GridSizeTestCase((None, None), (3, 2)),
-                GridSizeTestCase((3, None), (3, 2)),
-                GridSizeTestCase((None, 2), (3, 2)),
-                GridSizeTestCase((3, 2), (3, 2)),
-                GridSizeTestCase((None, 5), (1, 5)),
-                GridSizeTestCase((5, 1), (5, 1)),
-                GridSizeTestCase((5, 1), (5, 1)),
-            ]
-        )
+        super().test_vis_gallery(self.DEFAULT_GRID_SIZE_TEST_CASES)
+
+
+class CaptionVisualizerTest(TestCase, VisualizerTestBase):
+    @classmethod
+    def setUpClass(cls):
+        cls.subset = "train"
+        cls.items = [
+            DatasetItem(
+                "image_%03d" % img_idx,
+                subset=cls.subset,
+                media=Image(data=np.ones((4, 6, 3))),
+                annotations=[
+                    Caption(
+                        caption="".join([str(label_idx)] * 10),
+                        id=img_idx * img_idx + label_idx,
+                        attributes={},
+                    )
+                    for label_idx in range(img_idx)
+                ],
+            )
+            for img_idx in range(1, 6)
+        ]
+        cls.dataset = Dataset.from_iterable(cls.items)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    @mock.patch("datumaro.components.visualizer.Visualizer._draw_caption")
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        super().test_vis_one_sample(mocked, check_z_order=False)
+
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_vis_gallery(self):
+        super().test_vis_gallery(self.DEFAULT_GRID_SIZE_TEST_CASES)
