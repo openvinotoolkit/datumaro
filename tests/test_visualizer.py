9c43a75bc45f97eb43bda06741118b59925de377
Kim, Vinnam <vinnam.kim@intel.com>
2022-10-19 18:10:11 +0900

    Refactor test_visualizer.py
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- tests/test_visualizer.py
+++ tests/test_visualizer.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from unittest import TestCase, mock
 
 import numpy as np
@@ -12,7 +13,65 @@ from datumaro.components.visualizer import Visualizer
 from .requirements import Requirements, mark_requirement
 
 
-class BboxVisualizerTest(TestCase):
+@dataclass
+class GridSizeTestCase:
+    infer_grid_size: tuple
+    expected_grid_size: tuple
+
+
+class VisualizerTestBase:
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        visualizer = Visualizer(self.dataset)
+
+        for item in self.items:
+            visualizer.vis_one_sample(item.id, self.subset)
+
+            # Check count
+            assert mocked.call_count == len(item.annotations)
+
+            # Check z-order
+            called_z_order = [call[0][0].z_order for call in mocked.call_args_list]
+            assert sorted(called_z_order) == called_z_order
+
+            mocked.reset_mock()
+
+        # Unknown id
+        with self.assertRaises(Exception):
+            visualizer.vis_one_sample("unknown", self.subset)
+
+        # Unknown subset
+        for item in self.items:
+            with self.assertRaises(Exception):
+                visualizer.vis_one_sample(item.id, "unknown")
+
+    def test_vis_gallery(self, test_cases: GridSizeTestCase):
+        visualizer = Visualizer(self.dataset)
+
+        # Too small nrows and ncols
+        ids = [item.id for item in self.items]
+
+        cnt = 1
+        while cnt * cnt < len(ids):
+            cnt += 1
+
+        with self.assertRaises(Exception):
+            small_grid_size = (cnt - 1, cnt - 1)
+            visualizer.vis_gallery(ids, self.subset, small_grid_size)
+
+        # Infer grid size for 5 items
+        def _check(infer_grid_size, expected_grid_size):
+            expected_nrows, expected_ncols = expected_grid_size
+            fig = visualizer.vis_gallery(ids, self.subset, infer_grid_size)
+            self.assertIsInstance(fig, Figure)
+            grid_spec = fig.axes[0].get_gridspec()
+            self.assertEqual(grid_spec.nrows, expected_nrows)
+            self.assertEqual(grid_spec.ncols, expected_ncols)
+
+        for test_case in test_cases:
+            _check(test_case.infer_grid_size, test_case.expected_grid_size)
+
+
+class BboxVisualizerTest(TestCase, VisualizerTestBase):
     @classmethod
     def setUpClass(cls):
         cls.subset = "train"
@@ -41,57 +100,20 @@ class BboxVisualizerTest(TestCase):
         cls.dataset = Dataset.from_iterable(cls.items)
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
-    def test_vis_one_sample(self):
-        visualizer = Visualizer(self.dataset)
-
-        for item in self.items:
-            with mock.patch("datumaro.components.visualizer.Visualizer._draw_bbox") as mocked:
-                visualizer.vis_one_sample(item.id, self.subset)
-
-                # Check count
-                assert mocked.call_count == len(item.annotations)
-
-                # Check z-order
-                called_z_order = [call[0][0].z_order for call in mocked.call_args_list]
-                assert sorted(called_z_order) == called_z_order
-
-        # Unknown id
-        with self.assertRaises(Exception):
-            visualizer.vis_one_sample("unknown", self.subset)
-
-        # Unknown subset
-        for item in self.items:
-            with self.assertRaises(Exception):
-                visualizer.vis_one_sample(item.id, "unknown")
+    @mock.patch("datumaro.components.visualizer.Visualizer._draw_bbox")
+    def test_vis_one_sample(self, mocked: mock.MagicMock):
+        super().test_vis_one_sample(mocked)
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_vis_gallery(self):
-        visualizer = Visualizer(self.dataset)
-
-        # Too small nrows and ncols
-        ids = [item.id for item in self.items]
-
-        cnt = 1
-        while cnt * cnt < len(ids):
-            cnt += 1
-
-        with self.assertRaises(Exception):
-            small_grid_size = (cnt - 1, cnt - 1)
-            visualizer.vis_gallery(ids, self.subset, small_grid_size)
-
-        # Infer grid size for 5 items
-        def _check(infer_grid_size, expected_nrows, expected_ncols):
-            fig = visualizer.vis_gallery(ids, self.subset, infer_grid_size)
-            self.assertIsInstance(fig, Figure)
-            grid_spec = fig.axes[0].get_gridspec()
-            self.assertEqual(grid_spec.nrows, expected_nrows)
-            self.assertEqual(grid_spec.ncols, expected_ncols)
-
-        _check((None, None), 3, 2)
-        _check((3, None), 3, 2)
-        _check((None, 2), 3, 2)
-        _check((3, 2), 3, 2)
-
-        _check((None, 5), 1, 5)
-        _check((5, 1), 5, 1)
-        _check((5, 1), 5, 1)
+        super().test_vis_gallery(
+            [
+                GridSizeTestCase((None, None), (3, 2)),
+                GridSizeTestCase((3, None), (3, 2)),
+                GridSizeTestCase((None, 2), (3, 2)),
+                GridSizeTestCase((3, 2), (3, 2)),
+                GridSizeTestCase((None, 5), (1, 5)),
+                GridSizeTestCase((5, 1), (5, 1)),
+                GridSizeTestCase((5, 1), (5, 1)),
+            ]
+        )
