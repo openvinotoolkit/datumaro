7b5ba55486ca1e5b62ec505158f1151c57788b61
Kim, Vinnam <vinnam.kim@intel.com>
2023-02-03 19:02:00 +0900

    Resolve issue #721
     - Fix model loading in datum explain
     - Add an integration test for datum explain
    
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

---
+++ tests/assets/rise/model_interp.py
@@ -0,0 +1,14 @@
+from datumaro.util.annotation_util import softmax
+from datumaro.components.annotation import AnnotationType, Label, LabelCategories
+
+
+def process_outputs(inputs, outputs):
+    # inputs = model input; array or images; shape = (1, H, W, C)
+    # outputs = model output; shape = (1, 3);
+    # results = conversion result; [[a score for label0, a score for label1, a score for label2]]
+
+    assert len(outputs) == 1
+
+    return [
+        [Label(label=label, attributes={"score": score}) for label, score in enumerate(outputs[0])]
+    ]
---
+++ tests/integration/cli/test_explain.py
@@ -0,0 +1,71 @@
+# Copyright (C) 2023 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+import os
+import os.path as osp
+from unittest import TestCase
+
+import numpy as np
+
+from datumaro.util.image import save_image
+from datumaro.util.test_utils import TestDir
+from datumaro.util.test_utils import run_datum as run
+
+from ...requirements import Requirements, mark_requirement
+
+from tests.utils.assets import get_test_asset_path
+
+
+class ExplainIntegrationScenarios(TestCase):
+    @mark_requirement(Requirements.DATUM_BUG_721)
+    def test_rise(self):
+        rise_dir = get_test_asset_path("rise")
+
+        with TestDir() as project_dir:
+            # Create project
+            run(self, "create", "-o", project_dir)
+
+            model_name = "my-model"
+
+            # Add model
+            run(
+                self,
+                "model",
+                "add",
+                "-n",
+                model_name,
+                "-l",
+                "openvino",
+                "-p",
+                project_dir,
+                "--",
+                "-d",
+                osp.join(rise_dir, "model.xml"),
+                "-w",
+                osp.join(rise_dir, "model.bin"),
+                "-i",
+                osp.join(rise_dir, "model_interp.py"),
+            )
+
+            input_fpath = osp.join(project_dir, "input", "input.jpg")
+            save_image(input_fpath, np.zeros([224, 224, 3], dtype=np.uint8), create_dir=True)
+
+            output_dir = osp.join(project_dir, "output")
+
+            # Run explain
+            run(
+                self,
+                "explain",
+                "-m",
+                model_name,
+                "-o",
+                output_dir,
+                "-p",
+                project_dir,
+                input_fpath,
+                "rise",
+            )
+
+            num_heatmap = len(os.listdir(output_dir))
+            self.assertTrue(num_heatmap > 0)
