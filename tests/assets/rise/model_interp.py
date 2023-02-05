d548a7eb23e064024a2cd74910666e5f08e4a9d6
Kim, Vinnam <vinnam.kim@intel.com>
2023-02-05 12:46:29 +0900

    Make model_interp.py to have arbitrary batch input shape
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- tests/assets/rise/model_interp.py
+++ tests/assets/rise/model_interp.py
@@ -3,15 +3,15 @@ from datumaro.util.annotation_util import softmax
 
 
 def process_outputs(inputs, outputs):
-    # inputs = model input; array or images; shape = (1, H, W, C)
-    # outputs = model output; shape = (1, 3);
-    # results = conversion result; [[a score for label0, a score for label1, a score for label2]]
-
-    assert len(outputs) == 1
+    # inputs = model input; array or images; shape = (B, H, W, C)
+    # outputs = model output; shape = (B, 3);
+    # results = conversion result;
+    # [B x [a score for label0, a score for label1, a score for label2]];
 
     return [
         [
             Label(label=label, attributes={"score": score})
-            for label, score in enumerate(softmax(outputs[0]))
+            for label, score in enumerate(softmax(output))
         ]
+        for output in outputs
     ]
