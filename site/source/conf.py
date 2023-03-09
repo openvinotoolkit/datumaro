2169a455d54863d04574bc14ddc73d8563a5d46a
Emily <emily.chun@intel.com>
2023-03-09 18:04:22 +0900

    Update to original one

--- site/source/conf.py
+++ site/source/conf.py
@@ -1,10 +1,14 @@
+# Copyright (C) 2021-2022 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup -------------------------------------------------------------- #
+# -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -19,33 +23,35 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 from datumaro.version import VERSION
 
-# -- Project information ----------------------------------------------------- #
+# -- Project information -----------------------------------------------------
 
-project = 'Datumaro'
-copyright = '2023, Datumaro Contributors'
-author = 'Datumaro Contributors'
+project = "Datumaro"
+copyright = "2023, Datumaro Contributors"
+author = "Datumaro Contributors"
 
 # The full version, including alpha/beta/rc tags
 release = VERSION
 
-# -- General configuration --------------------------------------------------- #
+# -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon',  # Support for NumPy and Google style docstrings
-    'sphinx.ext.autodoc',  # Core library for html generation from docstrings
-    'sphinx.ext.viewcode',  # Find the source files
-    'sphinx_copybutton',  # Copy buttons for code blocks
-    'sphinx.ext.autosectionlabel',  # Refer sections its title
-    'sphinx.ext.intersphinx',  # Generate links to the documentation
+    "sphinx.ext.napoleon",  # Support for NumPy and Google style docstrings
+    "sphinx.ext.autodoc",  # Core library for html generation from docstrings
+    "sphinx.ext.viewcode",  # Find the source files
+    "sphinx_copybutton",  # Copy buttons for code blocks
+    "sphinx.ext.autosectionlabel",  # Refer sections its title
+    "sphinx.ext.intersphinx",  # Generate links to the documentation
     # of objects in external projects
-    'sphinxcontrib.mermaid',  # allows Mermaid graphs
+    "sphinxcontrib.mermaid",  # allows Mermaid graphs
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = [
+    "_templates",
+]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -53,25 +59,28 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
-# -- Options for HTML output ------------------------------------------------- #
-# The theme to use for HTML and HTML Help pages.  See the documentation for
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_theme = "pydata_sphinx_theme"
+html_static_path = [
+    "_static",
+]
 
 html_theme_options = {
-   'navbar_center': [],
-   'logo': {
-      'image_light': 'datumaro-logo.png',
-      'image_dark': 'datumaro-logo.png',
+   "navbar_center": [],
+   "logo": {
+      "image_light": "datumaro-logo.png",
+      "image_dark": "datumaro-logo.png",
    }
 }
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
 # -- Extension configuration -------------------------------------------------
