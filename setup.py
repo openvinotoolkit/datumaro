3aa91bc6c52fd939f87bae1b028e9680d102be14
wonjuleee <wonju@intel.com>
2023-03-14 12:20:34 +0900

    restore etup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@
 import os
 import os.path as osp
 import re
-import subprocess
-import sys
-from distutils.command.build_py import build_py as _build_py
-from distutils.command.clean import clean as _clean
-from distutils.spawn import find_executable
 from distutils.util import strtobool
 
 import setuptools
@@ -38,48 +33,6 @@ def find_version(project_dir=None):
 
     version = version_text[match.start(1) : match.end(1)]
     return version
-
-
-# Find the Protocol Compiler.
-if "PROTOC" in os.environ and os.path.exists(os.environ["PROTOC"]):
-    protoc = os.environ["PROTOC"]
-else:
-    protoc = find_executable("protoc")
-
-# List of all .proto files
-proto_src = ["datumaro/plugins/data_formats/ava/ava_label.proto"]
-
-
-class build_py(_build_py):
-    def run(self):
-        for src in proto_src:
-            if not os.path.exists(src):
-                sys.stderr.write("Can't find required file: %s\n" % src)
-                sys.exit(-1)
-
-            if protoc is None:
-                sys.stderr.write(
-                    "protoc is not installed nor found. Please compile it "
-                    "or install the binary package.\n"
-                )
-                sys.exit(-1)
-
-            protoc_command = [protoc, "-I.", "--python_out=.", src]
-            if subprocess.call(protoc_command) != 0:
-                sys.exit(-1)
-        _build_py.run(self)
-
-
-class clean(_clean):
-    def run(self):
-        # Delete generated files in the code tree.
-        for dirpath, _, filenames in os.walk("."):
-            for filename in filenames:
-                filepath = os.path.join(dirpath, filename)
-                if filepath.endswith("_pb2.py"):
-                    os.remove(filepath)
-        # _clean is an old-style class, so super() doesn't work.
-        _clean.run(self)
 
 
 CORE_REQUIREMENTS_FILE = "requirements-core.txt"
@@ -145,7 +98,7 @@ setuptools.setup(
             "datum=datumaro.cli.__main__:main",
         ],
     },
-    cmdclass={"build_ext": build_ext, "build_py": build_py, "clean": clean},
+    cmdclass={"build_ext": build_ext},
     package_data={"datumaro.plugins.synthetic_data": ["background_colors.txt"]},
     include_package_data=True,
 )
