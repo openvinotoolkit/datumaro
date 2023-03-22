ca1f6b5cf87c511c9aefc1c79900221c991543c1
Kim, Vinnam <vinnam.kim@intel.com>
2023-03-22 11:20:31 +0900

    Add prefix to secret key
     - Remove redundant file also
    
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- datumaro/components/crypter.py
+++ datumaro/components/crypter.py
@@ -7,19 +7,29 @@ from typing import Optional, Union
 
 from cryptography.fernet import Fernet, InvalidToken
 
+from datumaro.components.errors import DatumaroError
+
 
 class Crypter:
-    FERNET_KEY_LEN = 44
+    # Prefix (datum-) = 6 and Fernet = 44, 6 + 44 = 50
+    FERNET_KEY_LEN = 50
+    KEY_PREFIX = b"datum-"
+    KEY_PREFIX_LEN = len(KEY_PREFIX)
 
     def __init__(self, key: Union[str, bytes]) -> None:
         if isinstance(key, str):
             key = key.encode()
-        self._key = key
+        if len(key) != self.FERNET_KEY_LEN:
+            raise DatumaroError(
+                f"Key length should be {self.FERNET_KEY_LEN}, "
+                f"but your key length is {len(key)} (key={key})."
+            )
+        self._key = key[self.KEY_PREFIX_LEN :]
         self._fernet = Fernet(self._key)
 
     @property
     def key(self) -> bytes:
-        return self._key
+        return self.KEY_PREFIX + self._key
 
     def decrypt(self, msg: bytes) -> bytes:
         return self._fernet.decrypt(msg)
@@ -29,17 +39,17 @@ class Crypter:
 
     def handshake(self, key: bytes) -> bool:
         try:
-            return self.decrypt(key) == self._key
+            return self.decrypt(key) == self.key
         except InvalidToken as e:
             log.debug(e)
             return False
 
-    @staticmethod
-    def gen_key(key: Optional[bytes] = None) -> bytes:
+    @classmethod
+    def gen_key(cls, key: Optional[bytes] = None) -> bytes:
         """If "key" is not None, return the different key with "key"."""
-        _key = Fernet.generate_key()
+        _key = cls.KEY_PREFIX + Fernet.generate_key()
         while _key == key:
-            _key = Fernet.generate_key()
+            _key = cls.KEY_PREFIX + Fernet.generate_key()
         return _key
 
     def __eq__(self, __o: object) -> bool:
@@ -66,6 +76,10 @@ class NullCrypter(Crypter):
 
     def handshake(self, key: bytes) -> bool:
         return self.decrypt(key) == b""
+
+    @property
+    def key(self) -> None:
+        return None
 
 
 NULL_CRYPTER = NullCrypter()
