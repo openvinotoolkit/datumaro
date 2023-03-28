885029011a1a6357d5ac68c765989cdb9fe374c0
Kim, Vinnam <vinnam.kim@intel.com>
2023-03-28 16:47:58 +0900

    Set longer timeout length
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- datumaro/plugins/data_formats/datumaro_binary/format.py
+++ datumaro/plugins/data_formats/datumaro_binary/format.py
@@ -23,7 +23,7 @@ class DatumaroBinaryPath:
     SECRET_KEY_FILE = "secret_key.txt"
 
     MAX_BLOB_SIZE = 2**20  # 1 Mega bytes
-    MP_TIMEOUT = 30.0  # 30 secs
+    MP_TIMEOUT = 300.0  # 5 minutes
 
     @classmethod
     def check_signature(cls, signature: str):
