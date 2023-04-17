700e3b4be5c8541014c7bb1abbb10f224304496d
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2023-04-17 13:49:47 +0900

    fix: add __init__
    Signed-off-by: Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>

---
+++ datumaro/plugins/data_formats/arrow/mapper/__init__.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2023 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+# ruff: noqa: F405
+
+from .dataset_item import *
+from .media import *
+
+__all__ = [
+    # dataset_item
+    "DatasetItemMapper",
+    # media
+    "MediaMapper",
+]
