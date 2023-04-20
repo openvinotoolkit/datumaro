4eaf3c15f3a6b1cd25056e0ef555c1b79bf57605
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2023-04-20 15:13:48 +0900

    docs: fix validate
    Signed-off-by: Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>

--- datumaro/cli/commands/validate.py
+++ datumaro/cli/commands/validate.py
@@ -35,8 +35,11 @@ def build_parser(parser_ctor=argparse.ArgumentParser):
         project's working tree is used.|n
         |n
         Examples:|n
-        - Validate a project's subset as a classification dataset:|n |n
-        |s|s%(prog)s -t classification -s train
+        - Validate a project's subset as a classification dataset:|n
+        |s|s%(prog)s -t classification -s train|n
+        |n
+        - Give warning when imbalance ratio of data with classification task over 40:|n
+        |s|s%(prog)s -p <path/to/project/> -t classification -- -ir 40
         """,
         formatter_class=MultilineFormatter,
     )
