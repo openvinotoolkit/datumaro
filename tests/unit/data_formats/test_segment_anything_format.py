efd3eff7e7e4606773ce97a2dd132be7842e2f4d
Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>
2023-05-19 13:56:22 +0900

    test: use base test methods in baseclasee
    Signed-off-by: Inhyuk Andy Cho <andy.inhyuk.jo@intel.com>

--- tests/unit/data_formats/test_segment_anything_format.py
+++ tests/unit/data_formats/test_segment_anything_format.py
@@ -3,15 +3,11 @@
 # SPDX-License-Identifier: MIT
 
 import os
-from typing import Any, Dict, Optional
 
 import pytest
 
 from datumaro.components.annotation import Bbox, RleMask
 from datumaro.components.dataset import Dataset, DatasetItem
-from datumaro.components.environment import DEFAULT_ENVIRONMENT
-from datumaro.components.exporter import Exporter
-from datumaro.components.importer import Importer
 from datumaro.components.media import Image
 from datumaro.plugins.data_formats.segment_anything import (
     SegmentAnythingExporter,
@@ -24,16 +20,17 @@ from .base import TestDataFormatBase
 from tests.utils.assets import get_test_asset_path
 from tests.utils.test_utils import compare_datasets
 
+
 DATASET_DIR = get_test_asset_path("segment_anything_dataset")
 
 
 class SegmentAnythingTest(TestDataFormatBase):
     IMPORTER = SegmentAnythingImporter
     EXPORTER = SegmentAnythingExporter
-    USE_TEST_CAN_EXPORT_AND_IMPORT = False
+    USE_TEST_CAN_EXPORT_AND_IMPORT = True
 
     @pytest.fixture
-    def fxt_dataset(self):
+    def fxt_expected_dataset(self):
         expected_dataset = Dataset.from_iterable(
             [
                 DatasetItem(
@@ -112,36 +109,3 @@ class SegmentAnythingTest(TestDataFormatBase):
     @pytest.fixture
     def fxt_dataset_dir(self) -> str:
         return get_test_asset_path("segment_anything_dataset")
-
-    def test_can_detect(self, fxt_dataset_dir: str):
-        assert DEFAULT_ENVIRONMENT.detect_dataset(fxt_dataset_dir) == ["segment_anything"]
-
-    def test_can_import(
-        self,
-        fxt_dataset_dir: str,
-        fxt_dataset: Dataset,
-        fxt_import_kwargs: Dict[str, Any],
-        request: pytest.FixtureRequest,
-    ):
-        return super().test_can_import(
-            fxt_dataset_dir,
-            fxt_dataset,
-            fxt_import_kwargs,
-            request,
-        )
-
-    def test_can_export_and_import(
-        self,
-        fxt_dataset: Dataset,
-        test_dir: str,
-        fxt_import_kwargs: Dict[str, Any],
-        fxt_export_kwargs: Dict[str, Any],
-        request: pytest.FixtureRequest,
-    ):
-        return super().test_can_export_and_import(
-            fxt_dataset,
-            test_dir,
-            fxt_import_kwargs,
-            fxt_export_kwargs,
-            request,
-        )
