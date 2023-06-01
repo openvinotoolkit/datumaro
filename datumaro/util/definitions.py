35d8b4a4380a25aa8ef031147870b73cde13b006
Vinnam Kim <vinnam.kim@intel.com>
2023-06-01 16:49:20 +0900

    Change default model directory of FractalImageGenerator (#1032)
    - Ticket no. 111922
    - Change default model directory of FractalImageGenerator to be aligned
    with `OpenvinoLauncher`'s cache directory.
    - This will prevent storing the model files to the current working
    directory.
    
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- datumaro/util/definitions.py
+++ datumaro/util/definitions.py
@@ -2,8 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 
+import os
+import os.path as osp
 from typing import Tuple
 
 DEFAULT_SUBSET_NAME = "default"
 BboxIntCoords = Tuple[int, int, int, int]  # (x, y, w, h)
 SUBSET_NAME_BLACKLIST = {"labels", "images", "annotations", "instances"}
+
+_CACHE_DIR = osp.expanduser(os.getenv("XDG_CACHE_HOME", osp.join("~", ".cache")))
+DATUMARO_CACHE_DIR = osp.join(_CACHE_DIR, "datumaro")
+
+if not osp.exists(DATUMARO_CACHE_DIR):
+    os.makedirs(DATUMARO_CACHE_DIR)
