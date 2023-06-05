f8c016a0d59cc534f6faac8625d90d0a72dbbcda
Wonju Lee <wonju.lee@intel.com>
2023-06-05 21:37:19 +0900

    enable yolov5, v7, v8

--- datumaro/plugins/data_formats/roboflow/base.py
+++ datumaro/plugins/data_formats/roboflow/base.py
@@ -4,17 +4,20 @@
 
 import os
 import os.path as osp
-from typing import List, Optional, Union
+from typing import List, Optional
 from xml.etree import ElementTree
 
 from datumaro.components.annotation import AnnotationType, LabelCategories
+from datumaro.components.dataset_base import DatasetItem, SubsetBase
 from datumaro.components.importer import ImportContext
+from datumaro.components.media import Image
 from datumaro.plugins.data_formats.coco.base import _CocoBase
 from datumaro.plugins.data_formats.coco.format import CocoImporterType, CocoTask
 from datumaro.plugins.data_formats.voc.base import VocBase
 from datumaro.plugins.data_formats.voc.format import VocImporterType, VocTask
-from datumaro.plugins.data_formats.yolo.base import YoloUltralyticsBase
-from datumaro.util.image import ImageMeta
+from datumaro.plugins.data_formats.yolo.base import YoloStrictBase, YoloUltralyticsBase
+from datumaro.util.image import IMAGE_EXTENSIONS
+from datumaro.util.os_util import find_files
 
 
 class RoboflowCocoBase(_CocoBase):
@@ -82,12 +85,44 @@ class RoboflowVocBase(VocBase):
         return [os.path.splitext(file)[0] for file in os.listdir(path) if file.endswith(".xml")]
 
 
-class RoboflowYoloBase(YoloUltralyticsBase):
+class RoboflowYoloBase(YoloUltralyticsBase, SubsetBase):
     def __init__(
         self,
         config_path: str,
-        image_info: Union[None, str, ImageMeta] = None,
         urls: Optional[List[str]] = None,
-        **kwargs,
+        *,
+        subset: Optional[str] = None,
+        ctx: Optional[ImportContext] = None,
     ) -> None:
-        super().__init__(config_path, image_info, urls, **kwargs)
+        SubsetBase.__init__(self, subset=subset, ctx=ctx)
+
+        rootpath = osp.dirname(config_path)
+
+        # Init label categories
+        label_categories = self._load_categories(osp.join(rootpath, self.META_FILE))
+        self._categories = {AnnotationType.label: label_categories}
+
+        # Parse dataset items
+        def _get_fname(fpath: str) -> str:
+            return osp.splitext(osp.basename(fpath))[0]
+
+        img_files = {
+            _get_fname(img_file): img_file
+            for img_file in find_files(rootpath, IMAGE_EXTENSIONS, recursive=True, max_depth=2)
+            if osp.split(osp.relpath(osp.dirname(img_file), rootpath))[0] == self._subset
+        }
+
+        for url in urls:
+            try:
+                fname = _get_fname(url)
+                img = Image.from_file(path=img_files[fname])
+                anns = YoloStrictBase._parse_annotations(
+                    url,
+                    img,
+                    label_categories=label_categories,
+                )
+                self._items.append(
+                    DatasetItem(id=fname, subset=self._subset, media=img, annotations=anns)
+                )
+            except Exception as e:
+                self._ctx.error_policy.report_item_error(e, item_id=(fname, self._subset))
--- src/datumaro/plugins/data_formats/yolo/base.py
+++ src/datumaro/plugins/data_formats/yolo/base.py
@@ -306,15 +306,12 @@ class YoloLooseBase(SubsetBase):
     ) -> None:
         super().__init__(subset=subset, ctx=ctx)
 
-        print(config_path)
-
-        # if not osp.isdir(config_path):
-        #     raise DatasetImportError(f"{config_path} should be a directory.")
+        if not osp.isdir(config_path):
+            raise DatasetImportError(f"{config_path} should be a directory.")
 
         rootpath = config_path
-        self._path = rootpath
 
-        # self._image_info = YoloStrictBase.parse_image_info(rootpath, image_info)
+        self._image_info = YoloStrictBase.parse_image_info(rootpath, image_info)
 
         # Init label categories
         label_categories = self._load_categories(osp.join(rootpath, self.META_FILE))
@@ -369,9 +366,13 @@ class YoloUltralyticsBase(YoloLooseBase):
 
         with open(names_path, "r") as fp:
             loaded = yaml.safe_load(fp.read())
-            for label_name in loaded["names"].values():
-                label_categories.add(label_name)
-
-        print(label_categories)
+            if isinstance(loaded["names"], list):
+                for label_name in loaded["names"]:
+                    label_categories.add(label_name)
+            elif isinstance(loaded["names"], dict):
+                for label_name in loaded["names"].values():
+                    label_categories.add(label_name)
+            else:
+                raise DatasetImportError(f"Can't read dataset category file '{names_path}'")
 
         return label_categories
