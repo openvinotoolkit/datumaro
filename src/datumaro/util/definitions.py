8d09514f184c358167c4c3aa608235869647931d
Kim, Vinnam <vinnam.kim@intel.com>
2023-07-11 07:43:29 +0900

    Add missing return
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- src/datumaro/util/definitions.py
+++ src/datumaro/util/definitions.py
@@ -14,7 +14,7 @@ SUBSET_NAME_BLACKLIST = {"labels", "images", "annotations", "instances"}
 
 def get_datumaro_cache_dir(
     _CACHE_DIR: str = osp.expanduser(os.getenv("XDG_CACHE_HOME", osp.join("~", ".cache")))
-):
+) -> str:
     """Get DATUMARO_CACHE_DIR. If it does not exists, create it."""
     DATUMARO_CACHE_DIR = osp.join(_CACHE_DIR, "datumaro")
 
@@ -23,3 +23,5 @@ def get_datumaro_cache_dir(
             os.makedirs(DATUMARO_CACHE_DIR)
     except Exception as e:
         log.error(f"Cannot create DATUMARO_CACHE_DIR={DATUMARO_CACHE_DIR} since {e}.")
+
+    return DATUMARO_CACHE_DIR
