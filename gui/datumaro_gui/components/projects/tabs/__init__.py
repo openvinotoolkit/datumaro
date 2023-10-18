252c3e5bf56f52add72625a11859666c93b80917
Yi, Jihyeon <jihyeon.yi@intel.com>
2023-10-18 20:41:42 +0900

    fix import bug regarding unloaded_zip, refactor single dataset functionality

---
+++ gui/datumaro_gui/components/projects/tabs/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019-2023 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
+from .analyze import main as call_analyze
+from .explore import main as call_explore
+from .export import main as call_export
+from .general import main as call_general
+from .transform import main as call_transform
+from .validate import main as call_validate
+from .visualize import main as call_visualize
+
+__all__ = [
+    "call_analyze",
+    "call_explore",
+    "call_export",
+    "call_general",
+    "call_transform",
+    "call_validate",
+    "call_visualize",
+]
