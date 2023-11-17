0149656ac9c483622cf376b67e8f7f0213afc568
Kim, Vinnam <vinnam.kim@intel.com>
2023-11-17 16:24:27 +0900

    Add ImageColorScale context manager
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- tests/unit/test_image.py
+++ tests/unit/test_image.py
@@ -1,8 +1,13 @@
+# Copyright (C) 2019-2023 Intel Corporation
+#
+# SPDX-License-Identifier: MIT
+
 import os.path as osp
 from itertools import product
 from unittest import TestCase
 
 import numpy as np
+import pytest
 
 import datumaro.util.image as image_module
 
@@ -13,10 +18,10 @@ from tests.utils.test_utils import TestDir
 
 class ImageOperationsTest(TestCase):
     def setUp(self):
-        self.default_backend = image_module._IMAGE_BACKEND
+        self.default_backend = image_module._IMAGE_BACKEND.get()
 
     def tearDown(self):
-        image_module._IMAGE_BACKEND = self.default_backend
+        image_module._IMAGE_BACKEND.set(self.default_backend)
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_save_and_load_backends(self):
@@ -29,10 +34,10 @@ class ImageOperationsTest(TestCase):
                     src_image = np.random.randint(0, 255 + 1, (2, 4, c))
                 path = osp.join(test_dir, "img.png")  # lossless
 
-                image_module._IMAGE_BACKEND = save_backend
+                image_module._IMAGE_BACKEND.set(save_backend)
                 image_module.save_image(path, src_image, jpeg_quality=100)
 
-                image_module._IMAGE_BACKEND = load_backend
+                image_module._IMAGE_BACKEND.set(load_backend)
                 dst_image = image_module.load_image(path)
 
                 self.assertTrue(
@@ -49,10 +54,10 @@ class ImageOperationsTest(TestCase):
             else:
                 src_image = np.random.randint(0, 255 + 1, (2, 4, c))
 
-            image_module._IMAGE_BACKEND = save_backend
+            image_module._IMAGE_BACKEND.set(save_backend)
             buffer = image_module.encode_image(src_image, ".png", jpeg_quality=100)  # lossless
 
-            image_module._IMAGE_BACKEND = load_backend
+            image_module._IMAGE_BACKEND.set(load_backend)
             dst_image = image_module.decode_image(buffer)
 
             self.assertTrue(
@@ -71,3 +76,24 @@ class ImageOperationsTest(TestCase):
             path = osp.join(test_dir, "some", "path.jpg")
             image_module.save_image(path, np.ones((5, 4, 3)), create_dir=True)
             self.assertTrue(osp.isfile(path))
+
+
+class ImageDecodeTest:
+    @pytest.fixture
+    def fxt_img_four_channels(self) -> np.ndarray:
+        return np.random.randint(low=0, high=256, size=(5, 4, 4), dtype=np.uint8)
+
+    def test_decode_image_context(self, fxt_img_four_channels: np.ndarray):
+        img_bytes = image_module.encode_image(fxt_img_four_channels, ".png")
+
+        # 3 channels from ImageColorScale.COLOR
+        with image_module.decode_image_context(image_module.ImageColorScale.COLOR):
+            img_decoded = image_module.decode_image(img_bytes)
+            assert img_decoded.shape[-1] == 3
+            assert np.allclose(fxt_img_four_channels[:, :, :3], img_decoded)
+
+        # 4 channels from ImageColorScale.UNCHANGED
+        with image_module.decode_image_context(image_module.ImageColorScale.UNCHANGED):
+            img_decoded = image_module.decode_image(img_bytes)
+            assert img_decoded.shape[-1] == 4
+            assert np.allclose(fxt_img_four_channels, img_decoded)
