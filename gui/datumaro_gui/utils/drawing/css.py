1f5592fa0ee65fbbc2f19ad8a7f8201960c493e0
sooahleex <sooah.lee@intel.com>
2023-11-22 16:58:01 +0900

    Update transform for single dataset

--- gui/datumaro_gui/utils/drawing/css.py
+++ gui/datumaro_gui/utils/drawing/css.py
@@ -1,3 +1,5 @@
+import re
+
 # Define a custom CSS style
 custom_css = """
 <style>
@@ -17,7 +19,8 @@ box_style = """
     border-radius: 0.4rem;
     color: white;
     padding: 0.5rem;
-    margin-bottom: 1rem;
+    margin-top: 0.5rem;
+    margin-bottom: 0.5rem;
     }
     .bold {
     padding-left: 1rem;
@@ -28,6 +31,9 @@ box_style = """
     }
     .blue {
     background-color: lightblue;
+    }
+    .yellow {
+    background-color: rgba(255,218,138,.8);
     }
     .lightgrayish {
     background-color: #B0C4DE;
@@ -46,6 +52,13 @@ box_style = """
     max-width: 1000px;
     margin: "5px 5px 5px 5px";
     }
+    .stat_highlight {
+    border-radius: 0.4rem;
+    color: white;
+    padding: 5px;
+    width: auto;
+    margin: "5px 5px 5px 5px";
+    }
 """
 
 # Define CSS styles for the buttons
@@ -56,3 +69,33 @@ btn_style = """
     padding-bottom: 1px;
     }
 """
+
+
+def parse_css_string(css_string):
+    css_dict = {}
+    # Use regular expressions to extract CSS class names and styles
+    matches = re.findall(r"\.(.*?)\s*{([^}]*)}", css_string)
+    for match in matches:
+        class_name, class_styles = match
+        # Convert each class's styles to a dictionary and store
+        style_dict = {}
+        for style_pair in class_styles.split(";"):
+            style_pair = style_pair.strip()
+            if style_pair:
+                key, value = style_pair.split(":")
+                style_dict[key.strip()] = value.strip()
+        css_dict[class_name.strip()] = style_dict
+    return css_dict
+
+
+def apply_css_styles(css_string, *class_names):
+    css_dict = parse_css_string(css_string)
+    class_styles = [css_dict.get(class_name, {}) for class_name in class_names]
+    return merge_styles(*class_styles)
+
+
+def merge_styles(*style_dicts):
+    merged_styles = {}
+    for style_dict in style_dicts:
+        merged_styles.update(style_dict)
+    return merged_styles
