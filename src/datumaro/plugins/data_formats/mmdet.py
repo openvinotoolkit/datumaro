d1f57b83125b2390d8cb74275719814de56bc8f4
wonjulee <wonju.lee@intel.com>
2023-12-01 14:40:19 +0900

    separate format detector from coco

--- src/datumaro/plugins/data_formats/mmdet.py
+++ src/datumaro/plugins/data_formats/mmdet.py
@@ -7,6 +7,7 @@ from glob import glob
 from typing import Optional
 
 from datumaro.components.dataset_base import DEFAULT_SUBSET_NAME
+from datumaro.components.format_detection import FormatDetectionConfidence, FormatDetectionContext
 from datumaro.components.importer import ImportContext
 from datumaro.plugins.data_formats.coco.base import _CocoBase
 from datumaro.plugins.data_formats.coco.format import CocoImporterType, CocoTask
@@ -14,16 +15,30 @@ from datumaro.plugins.data_formats.coco.importer import CocoImporter
 
 
 class MmdetCocoImporter(CocoImporter):
+    @classmethod
+    def detect(
+        cls,
+        context: FormatDetectionContext,
+    ) -> FormatDetectionConfidence:
+        ann_paths = context.require_files("annotations/instances_*.json")
+
+        for ann_path in ann_paths:
+            subset_name = cls._get_subset_name(ann_path)
+
+            with context.require_any():
+                with context.alternative():
+                    image_files = osp.join(subset_name, "*.jpg")
+                    context.require_file(f"{image_files}")
+
+                    return FormatDetectionConfidence.MEDIUM
+
     def __call__(self, path, stream: bool = False, **extra_params):
         subset_paths = glob(osp.join(path, "**", "instances_*.json"), recursive=True)
 
         sources = []
         for subset_path in subset_paths:
-            parts = osp.splitext(osp.basename(subset_path))[0].split("instances_", maxsplit=1)
-            subset_name = parts[1] if len(parts) == 2 else DEFAULT_SUBSET_NAME
-
             options = dict(extra_params)
-            options["subset"] = subset_name
+            options["subset"] = self._get_subset_name(subset_path)
 
             if stream:
                 options["stream"] = True
@@ -31,6 +46,13 @@ class MmdetCocoImporter(CocoImporter):
             sources.append({"url": subset_path, "format": "mmdet_coco", "options": options})
 
         return sources
+
+    @classmethod
+    def _get_subset_name(cls, subset_path: str):
+        parts = osp.splitext(osp.basename(subset_path))[0].split("instances_", maxsplit=1)
+        subset_name = parts[1] if len(parts) == 2 else DEFAULT_SUBSET_NAME
+
+        return subset_name
 
 
 class MmdetCocoBase(_CocoBase):
