44e9284d1fc8a9a644f8f5a2d84461b983bc1682
sooahleex <sooah.lee@intel.com>
2023-12-11 16:55:27 +0900

    Add state keys in utils

--- gui/datumaro_gui/components/single/streamlit_app.py
+++ gui/datumaro_gui/components/single/streamlit_app.py
@@ -8,7 +8,12 @@ import os
 import streamlit as st
 import streamlit_antd_components as sac
 from datumaro_gui.utils.dataset.data_loader import DataRepo, SingleDatasetHelper
-from datumaro_gui.utils.dataset.state import file_selector, import_dataset, reset_state
+from datumaro_gui.utils.dataset.state import (
+    file_selector,
+    import_dataset,
+    reset_state,
+    single_state_keys,
+)
 from datumaro_gui.utils.drawing.css import custom_css
 from datumaro_gui.utils.readme import github_pypi_desc
 from streamlit import session_state as state
@@ -20,30 +25,11 @@ def main():
     st.write(github_pypi_desc)
     st.markdown(custom_css, unsafe_allow_html=True)
 
-    keys = [
-        "uploaded_file",
-        "data_helper",
-        "subset",
-        "stats_image",
-        "stats_anns",
-        "image_size_info",
-        "cls_summary",
-        "cls_anomaly_info",
-        "det_summary",
-        "det_anomaly_info",
-        "seg_summary",
-        "seg_anomaly_info",
-        "defined_label",
-        "undefined_label",
-        "defined_attr",
-        "undefined_attr",
-    ]
-
     filename = file_selector()
-    reset_state(keys, state)
+    reset_state(single_state_keys, state)
 
     if filename is not None:
-        reset_state(keys, state)
+        reset_state(single_state_keys, state)
         if filename.endswith(".zip"):
             data_repo = DataRepo()
             filename = data_repo.unzip_dataset(filename)
