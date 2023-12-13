859c398f03c48fcc8ca6dc2cb313e461b234a145
Kim, Vinnam <vinnam.kim@intel.com>
2023-12-13 12:21:36 +0900

    Add JsonSectionPageMapper to Rust API
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -4,6 +4,7 @@
 
 mod coco_page_mapper;
 mod datum_page_mapper;
+mod json_section_page_mapper;
 mod page_mapper;
 mod page_maps;
 mod test_helpers;
@@ -12,6 +13,7 @@ use pyo3::prelude::*;
 
 use crate::coco_page_mapper::CocoPageMapper;
 use crate::datum_page_mapper::DatumPageMapper;
+use crate::json_section_page_mapper::JsonSectionPageMapper;
 
 /// Datumaro Rust API
 #[pymodule]
@@ -19,6 +21,7 @@ use crate::datum_page_mapper::DatumPageMapper;
 fn rust_api(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_class::<CocoPageMapper>()?;
     m.add_class::<DatumPageMapper>()?;
+    m.add_class::<JsonSectionPageMapper>()?;
 
     Ok(())
 }
