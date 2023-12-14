dea513c38f893134afe3385307ef42ee52d6d559
Kim, Vinnam <vinnam.kim@intel.com>
2023-12-14 16:54:39 +0900

    Enhance Datumaro's detect() to be memory-bounded and performant
    Signed-off-by: Kim, Vinnam <vinnam.kim@intel.com>

--- src/datumaro/plugins/data_formats/segment_anything/importer.py
+++ src/datumaro/plugins/data_formats/segment_anything/importer.py
@@ -8,11 +8,13 @@ from typing import Dict, List, Optional
 from datumaro.components.format_detection import FormatDetectionConfidence, FormatDetectionContext
 from datumaro.components.importer import Importer
 from datumaro.errors import DatasetImportError
+from datumaro.rust_api import JsonSectionPageMapper
 from datumaro.util import parse_json
 
 
 class SegmentAnythingImporter(Importer):
     _N_JSON_TO_TEST = 10
+    _MAX_ANNOTATION_SECTION_BYTES = 100 * 1024 * 1024  # 100 MiB
 
     @classmethod
     def detect(
@@ -26,24 +28,37 @@ class SegmentAnythingImporter(Importer):
             with context.probe_text_file(
                 file, "Annotation format is not Segmentat-Anything format", is_binary_file=True
             ) as f:
-                anno = parse_json(f.read())
-                if (
-                    set(anno.keys()) != {"annotations", "image"}
-                    or (
-                        set(anno["image"].keys())
-                        != {
-                            "image_id",
-                            "width",
-                            "height",
-                            "file_name",
-                        }
-                    )
-                    or (
-                        anno["annotations"]
-                        and not {"id", "segmentation", "bbox"}.issubset(set(anno["annotations"][0]))
-                    )
-                ):
+                fpath = os.path.join(context.root_path, file)
+                page_mapper = JsonSectionPageMapper(fpath)
+                sections = page_mapper.sections()
+
+                if set(sections.keys()) != {"annotations", "image"}:
                     raise DatasetImportError
+
+                offset, size = sections["image"]["offset"], sections["image"]["size"]
+                f.seek(offset, 0)
+                img_contents = parse_json(f.read(size))
+
+                if set(img_contents.keys()) != {
+                    "image_id",
+                    "width",
+                    "height",
+                    "file_name",
+                }:
+                    raise DatasetImportError
+
+                offset, size = sections["annotations"]["offset"], sections["annotations"]["size"]
+
+                if size > cls._MAX_ANNOTATION_SECTION_BYTES:
+                    msg = f"Annotation section is too huge. It exceeded {cls._MAX_ANNOTATION_SECTION_BYTES} bytes."
+                    raise DatasetImportError(msg)
+
+                f.seek(offset, 0)
+                ann_contents = parse_json(f.read(size))
+
+                if not {"id", "segmentation", "bbox"}.issubset(set(ann_contents[0])):
+                    raise DatasetImportError
+
             if ctr > cls._N_JSON_TO_TEST:
                 break
 
