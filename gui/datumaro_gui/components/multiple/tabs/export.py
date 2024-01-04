1b5a6ef52e85d37330307618e25ebee85b24ba7e
sooahleex <sooah.lee@intel.com>
2024-01-04 15:20:23 +0900

    Add key for export tab

--- gui/datumaro_gui/components/multiple/tabs/export.py
+++ gui/datumaro_gui/components/multiple/tabs/export.py
@@ -49,7 +49,9 @@ def main():
     uploaded_file_1 = state["uploaded_file_1"]
     uploaded_file_2 = state["uploaded_file_2"]
     dataset_names = [uploaded_file_1, uploaded_file_2, "Merged Dataset"]
-    selected_dataset = st.selectbox("Select dataset to export : ", dataset_names, index=2)
+    selected_dataset = st.selectbox(
+        "Select dataset to export : ", dataset_names, index=2, key="sb_export_ds_mult"
+    )
     dataset_dict = {uploaded_file_1: data_helper_1, uploaded_file_2: data_helper_2}
 
     if selected_dataset == "Merged Dataset" and "data_helper_merged" not in state:
@@ -58,17 +60,20 @@ def main():
         data_helper_3: MultipleDatasetHelper = state["data_helper_merged"]
         dataset_dict["Merged Dataset"] = data_helper_3
 
-        selected_task = st.selectbox("Select a task to export:", tasks)
+        selected_task = st.selectbox("Select a task to export:", tasks, key="sb_task_export_mult")
         if selected_task:
-            selected_format = st.selectbox("Select a format to export:", formats[selected_task])
+            selected_format = st.selectbox(
+                "Select a format to export:", formats[selected_task], key="sb_format_export_mult"
+            )
 
         if selected_task and selected_format:
             selected_path = st.text_input(
                 "Select a path to export:",
                 value=osp.join(osp.expanduser("~"), "Downloads", "dataset.zip"),
+                key="ti_path_export_mult",
             )
 
-        export_btn = st.button("Export")
+        export_btn = st.button("Export", "btn_export_mult")
         if export_btn:
             data_helper = dataset_dict.get(selected_dataset, None)
             data_helper.export(selected_path, format=selected_format, save_media=True)
