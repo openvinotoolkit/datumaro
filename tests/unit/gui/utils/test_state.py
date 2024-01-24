02aae87611b32e3c06f8b99792f6cfe4d000d6d3
sooahleex <sooah.lee@intel.com>
2024-01-24 13:30:01 +0900

    Add unit test for save_dataset

--- tests/unit/gui/utils/test_state.py
+++ tests/unit/gui/utils/test_state.py
@@ -3,6 +3,11 @@
 # SPDX-License-Identifier: MIT
 
 import os
+import tempfile
+from datetime import datetime
+from unittest.mock import MagicMock, patch
+
+import pytest
 
 from tests.requirements import Requirements, mark_requirement
 
@@ -12,10 +17,19 @@ import sys
 
 sys.path.append(os.path.join(cwd, "gui"))
 from gui.datumaro_gui.utils.dataset.data_loader import DatasetHelper
-from gui.datumaro_gui.utils.dataset.state import get_download_folder_path, reset_state, reset_subset
+from gui.datumaro_gui.utils.dataset.state import (
+    get_download_folder_path,
+    reset_state,
+    reset_subset,
+    save_dataset,
+)
 
 
 class StateTest:
+    @pytest.fixture
+    def dataset_helper(self):
+        return MagicMock(spec=DatasetHelper)
+
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_get_download_folder_path(self):
         result = get_download_folder_path()
@@ -36,3 +50,21 @@ class StateTest:
         reset_state(keys, state)
         assert state["key1"] is None
         assert state["key2"] is None
+
+    @patch(
+        "gui.datumaro_gui.utils.dataset.state.get_download_folder_path",
+        return_value=tempfile.mkdtemp(),
+    )
+    @mark_requirement(Requirements.DATUM_GENERAL_REQ)
+    def test_save_dataset(self, mock_get_download_folder_path, dataset_helper):
+        filename = "test_dataset"
+        tmp_save_folder = mock_get_download_folder_path()
+        expected_save_path = os.path.join(
+            tmp_save_folder, f"{filename}_{datetime.now().strftime('%Y%m%d_%H%M%S')}"
+        )
+
+        save_dataset(dataset_helper, filename, tmp_save_folder)
+
+        dataset_helper.export.assert_called_once_with(
+            expected_save_path, format="datumaro", save_media=True
+        )
