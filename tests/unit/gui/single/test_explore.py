1f9d0cdefbc97c293beab10046e5f754394fb9cd
sooahleex <sooah.lee@intel.com>
2024-02-16 17:40:34 +0900

    Modify explore test with explore_dataset

--- tests/unit/gui/single/test_explore.py
+++ tests/unit/gui/single/test_explore.py
@@ -31,7 +31,7 @@ def run_explore():
 
     reset_state(single_state_keys, state)
 
-    dataset_dir = get_test_asset_path("datumaro_dataset")
+    dataset_dir = get_test_asset_path("explore_dataset")
     data_helper = SingleDatasetHelper(dataset_dir)
     state["data_helper"] = data_helper
     uploaded_file = os.path.basename(dataset_dir)
@@ -39,7 +39,7 @@ def run_explore():
 
     data_helper = state["data_helper"]
 
-    data_helper.import_dataset("datumaro")
+    data_helper.import_dataset("imagenet")
 
     tabs.call_explore()
 
@@ -65,7 +65,7 @@ class ExploreTest(TestCase):
 
         # multiselect
         multiselect_key = "ms_select_label"
-        options = ["bicycle", "car", "mary", "tom"]
+        options = ["bird", "cat", "dog", "monkey"]
         assert at.multiselect(multiselect_key).label == "Select Label(s)"
         assert at.multiselect(multiselect_key).options == options
         assert not at.multiselect(multiselect_key).value
@@ -91,9 +91,9 @@ class ExploreTest(TestCase):
 
         # Select label to explore
         multiselect_key = "ms_select_label"
-        at.multiselect(multiselect_key).select("bicycle").run()
-        at.multiselect(multiselect_key).select("car").run()
-        selected_labels = ["bicycle", "car"]
+        at.multiselect(multiselect_key).select("cat").run()
+        at.multiselect(multiselect_key).select("dog").run()
+        selected_labels = ["cat", "dog"]
         assert at.multiselect(multiselect_key).value == selected_labels
 
         # Check Empty query
@@ -106,7 +106,7 @@ class ExploreTest(TestCase):
         # Check query added
         assert len(at.session_state.explore_queries) == 1
         assert at.session_state.explore_queries[0].labels == selected_labels
-        assert at.checkbox("query_0").label == ":label: bicycle,car"
+        assert at.checkbox("query_0").label == ":label: cat,dog"
 
         # Check Remove button disabled
         button_key = "btn_remove_query"
@@ -126,7 +126,7 @@ class ExploreTest(TestCase):
         assert at.session_state.explore_results
 
         checkbox_key = "result_0"
-        assert at.checkbox(checkbox_key).label == "test-c"
+        assert at.checkbox(checkbox_key).label == "default-cat:ILSVRC2012_val_00004894"
 
         # Click result
         at.checkbox(checkbox_key).check().run()
@@ -153,22 +153,22 @@ class ExploreTest(TestCase):
 
         # Add unnecessary label to query
         multiselect_key = "ms_select_label"
-        at.multiselect(multiselect_key).select("mary").run()
+        at.multiselect(multiselect_key).select("bird").run()
 
         # Click add query button
         button_key = "btn_add_query"
         at.button(button_key).click().run()
 
         # Add real label to query
-        at.multiselect(multiselect_key).unselect("mary").run()
-        at.multiselect(multiselect_key).select("bicycle").run()
-        at.multiselect(multiselect_key).select("car").run()
+        at.multiselect(multiselect_key).unselect("bird").run()
+        at.multiselect(multiselect_key).select("cat").run()
+        at.multiselect(multiselect_key).select("dog").run()
         at.button(button_key).click().run()
 
         # Check query status
         assert len(at.session_state.explore_queries) == 2
-        assert at.checkbox("query_0").label == ":label: mary"
-        assert at.checkbox("query_1").label == ":label: bicycle,car"
+        assert at.checkbox("query_0").label == ":label: bird"
+        assert at.checkbox("query_1").label == ":label: cat,dog"
 
         # Remove unnecessary label from query
         at.checkbox("query_0").check().run()
@@ -190,17 +190,17 @@ class ExploreTest(TestCase):
         # Select subset to explore
         selectbox_key = "sb_select_subset_exp"
         assert at.selectbox(selectbox_key).label == "Select a subset:"
-        assert at.selectbox(selectbox_key).options == ["test", "train", "validation"]
+        assert at.selectbox(selectbox_key).options == ["default"]
 
-        selected_datasetitem_subset = "train"
+        selected_datasetitem_subset = "default"
         at.selectbox(selectbox_key).select(selected_datasetitem_subset).run()
         assert at.selectbox(selectbox_key).value == selected_datasetitem_subset
 
         # Select id to explore
         selectbox_key = "sb_select_id_exp"
         assert at.selectbox(selectbox_key).label == "Select a dataset item:"
-        assert at.selectbox(selectbox_key).options == ["a", "b"]
-        selected_datasetitem_id = "a"
+        assert len(at.selectbox(selectbox_key).options) == 24
+        selected_datasetitem_id = "bird:ILSVRC2012_val_00001563"
         at.selectbox(selectbox_key).select(selected_datasetitem_id).run()
 
         # Check Empty query
@@ -214,7 +214,10 @@ class ExploreTest(TestCase):
         assert len(at.session_state.explore_queries) == 1
         assert at.session_state.explore_queries[0].item.id == selected_datasetitem_id
         assert at.session_state.explore_queries[0].item.subset == selected_datasetitem_subset
-        assert at.checkbox("query_0").label == ":frame_with_picture: train-a"
+        assert (
+            at.checkbox("query_0").label
+            == ":frame_with_picture: default-bird:ILSVRC2012_val_00001563"
+        )
 
         # Check Remove button disabled
         button_key = "btn_remove_query"
@@ -234,7 +237,7 @@ class ExploreTest(TestCase):
         assert at.session_state.explore_results
 
         checkbox_key = "result_0"
-        assert at.checkbox(checkbox_key).label == "test-c"
+        assert at.checkbox(checkbox_key).label == "default-bird:ILSVRC2012_val_00001563"
 
         # Click result
         at.checkbox(checkbox_key).check().run()
@@ -258,81 +261,6 @@ class ExploreTest(TestCase):
     def test_explore_check_user_image(self):
         # TODO
         """Test if user image method of explore are worked out correctly."""
-        # at = AppTest.from_function(run_explore, default_timeout=600).run()
-
-        # # Select explore method
-        # radio_key = "rd_query_type"
-        # at.radio(radio_key).set_value("User Image").run()
-
-        # file_uploader_key = "explore_user_uploaded_file"
-        # assert at.file_uploader(file_uploader_key).label == 'Upload an image file'
-
-        # # Upload user image to explore
-        # at.file_uploader(file_uploader_key)
-
-        # assert at.selectbox(selectbox_key).options == ["test", "train", "validation"]
-
-        # selected_datasetitem_subset = 'train'
-        # at.selectbox(selectbox_key).select(selected_datasetitem_subset).run()
-        # assert at.selectbox(selectbox_key).value == selected_datasetitem_subset
-
-        # # Select id to explore
-        # selectbox_key = "sb_select_id_exp"
-        # assert at.selectbox(selectbox_key).label == "Select a dataset item:"
-        # assert at.selectbox(selectbox_key).options == ["a", "b"]
-        # selected_datasetitem_id = "a"
-        # at.selectbox(selectbox_key).select(selected_datasetitem_id).run()
-
-        # # Check Empty query
-        # assert not at.session_state.explore_queries
-
-        # # Click add query button
-        # button_key = "btn_add_query"
-        # at.button(button_key).click().run()
-
-        # # Check query added
-        # assert len(at.session_state.explore_queries) == 1
-        # assert at.session_state.explore_queries[0].item.id == selected_datasetitem_id
-        # assert at.session_state.explore_queries[0].item.subset == selected_datasetitem_subset
-        # assert at.checkbox("query_0").label == ':frame_with_picture: train-a'
-
-        # # Check Remove button disabled
-        # button_key = "btn_remove_query"
-        # assert at.button(button_key).disabled
-
-        # # Set topk as 1
-        # number_input_key = "ni_topk"
-        # at.number_input(number_input_key).set_value(1).run()
-        # assert at.number_input(number_input_key).value == 1
-
-        # # Check Empty explore results
-        # assert not at.session_state.explore_results
-
-        # # Click search
-        # button_key = "btn_search"
-        # at.button(button_key).click().run()
-        # assert at.session_state.explore_results
-
-        # checkbox_key = "result_0"
-        # assert at.checkbox(checkbox_key).label == 'test-c'
-
-        # # Click result
-        # at.checkbox(checkbox_key).check().run()
-        # select_slider_key = "ss_selected_alpha_for_result"
-        # assert at.select_slider(select_slider_key)
-        # assert at.select_slider(select_slider_key).options == list(map(str, range(0, 110, 10)))
-        # assert at.select_slider(select_slider_key).value == 20
-
-        # # Set slider value as 50
-        # at.select_slider(select_slider_key).set_value(50).run()
-        # assert at.select_slider(select_slider_key).value == 50
-
-        # # Check visualizer
-        # dataset = at.session_state.data_helper.dataset()
-        # visualizer = Visualizer(dataset, figsize=(8, 8), alpha=0.2, show_plot_title=False)
-        # selected_item = at.session_state.explore_results[0]
-        # fig = visualizer.vis_one_sample(selected_item.id, selected_item.subset)
-        # assert fig is not None
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_explore_check_text(self):
@@ -347,7 +275,7 @@ class ExploreTest(TestCase):
         text_input_key = "ti_query_text_exp"
         assert at.text_input(text_input_key).label == "Input text query:"
 
-        selected_text = "train"
+        selected_text = "bird"
         at.text_input(text_input_key).input(selected_text).run()
         assert at.text_input(text_input_key).value == selected_text
 
@@ -361,7 +289,7 @@ class ExploreTest(TestCase):
         # Check query added
         assert len(at.session_state.explore_queries) == 1
         assert at.session_state.explore_queries[0].text == selected_text
-        assert at.checkbox("query_0").label == ":speech_balloon: train"
+        assert at.checkbox("query_0").label == ":speech_balloon: bird"
 
         # Check Remove button disabled
         button_key = "btn_remove_query"
@@ -381,7 +309,7 @@ class ExploreTest(TestCase):
         assert at.session_state.explore_results
 
         checkbox_key = "result_0"
-        assert at.checkbox(checkbox_key).label == "test-c"
+        assert at.checkbox(checkbox_key).label == "default-bird:ILSVRC2012_val_00001563"
 
         # Click result
         at.checkbox(checkbox_key).check().run()
