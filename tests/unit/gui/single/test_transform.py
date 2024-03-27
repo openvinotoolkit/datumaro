401dd7be9de09ee36170fc07eb518f78b9321cea
sooahleex <sooah.lee@intel.com>
2024-03-27 15:53:50 +0900

    Match annotations in TransformRemove

--- tests/unit/gui/single/test_transform.py
+++ tests/unit/gui/single/test_transform.py
@@ -1061,15 +1061,11 @@ class TransformRemoveTest(TestCase):
             at.selectbox(selectbox_key).options
             == at.session_state.data_helper.subset_to_ids()[selected_subset]
         )
-        selected_id = at.selectbox(selectbox_key).value
-        selected_item = at.session_state.data_helper.dataset().get(selected_id, selected_subset)
 
         selectbox_key = "sb_select_ann_id_sin"
         assert at.selectbox(selectbox_key)
         assert at.selectbox(selectbox_key).label == "Select an annotation"
-        assert at.selectbox(selectbox_key).options == [
-            "All",
-        ] + sorted(list({str(ann.id) for ann in selected_item.annotations}))
+        assert at.selectbox(selectbox_key).options == ["All", "mary"]
 
         # button
         button_key = "btn_remove_item_remove_sin"
@@ -1120,7 +1116,13 @@ class TransformRemoveTest(TestCase):
         at.selectbox(selectbox_id_key).select("b").run()
 
         assert at.selectbox(selectbox_id_key).value == "b"
-        assert at.selectbox(selectbox_ann_id_key).options == ["All", "0", "1", "2", "3"]
+        assert at.selectbox(selectbox_ann_id_key).options == [
+            "All",
+            "car",
+            "bicycle",
+            "tom",
+            "mary",
+        ]
 
     @mark_requirement(Requirements.DATUM_GENERAL_REQ)
     def test_remove_item(self):
@@ -1489,8 +1491,8 @@ class TransformNDRTest:
         selectbox_key = "sb_select_subset_ndr_sin"
         assert at.selectbox(selectbox_key)
         assert at.selectbox(selectbox_key).label == "Select a subset to apply NDR:"
-        assert at.selectbox(selectbox_key).options == list(
-            at.session_state.data_helper.dataset().subsets().keys()
+        assert at.selectbox(selectbox_key).options == sorted(
+            list(at.session_state.data_helper.dataset().subsets().keys())
         )
 
         # text_input
