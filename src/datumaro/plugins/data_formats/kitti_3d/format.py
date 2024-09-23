c5e2df25a9c27817d709fbc178ea5a76d0badffc
Wonju Lee <wonju.lee@intel.com>
2024-09-23 19:04:20 +0900

    fix kitti3d base

--- src/datumaro/plugins/data_formats/kitti_3d/format.py
+++ src/datumaro/plugins/data_formats/kitti_3d/format.py
@@ -1,9 +1,8 @@
-# Copyright (C) 2021 Intel Corporation
+# Copyright (C) 2024 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 
 import os.path as osp
-from enum import Enum
 
 
 class Kitti3dPath:
@@ -11,30 +10,3 @@ class Kitti3dPath:
     IMAGE_DIR = "image_2"
     LABEL_DIR = "label_2"
     CALIB_DIR = "calib"
-    BUILTIN_ATTRS = {"occluded", "truncation", "occlusion"}
-    SPECIAL_ATTRS = {
-        "track_id",
-    }
-    ANNO_FILE = "tracklet_labels.xml"
-    NAME_MAPPING_FILE = "frame_list.txt"
-
-
-class PoseStates(Enum):
-    UNSET = 0
-    INTERP = 1
-    LABELED = 2
-
-
-class OcclusionStates(Enum):
-    OCCLUSION_UNSET = -1
-    VISIBLE = 0
-    PARTLY = 1
-    FULLY = 2
-
-
-class TruncationStates(Enum):
-    TRUNCATION_UNSET = -1
-    IN_IMAGE = 0
-    TRUNCATED = 1
-    OUT_IMAGE = 2
-    BEHIND_IMAGE = 99
